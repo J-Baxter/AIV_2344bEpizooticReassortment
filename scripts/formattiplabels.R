69be73f814d01923b3342c860ea18cf4947e1e6e
James Baxter <james.baxter@ed.ac.uk>
2023-12-15 17:45:11 +0000

    formatting tip labels with Rcpp functions and tidier code

--- scripts/formattiplabels.R
+++ scripts/formattiplabels.R
@@ -2,44 +2,30 @@ library(ape)
 library(tidyverse)
 library(treeio)
 library(TreeTools)
+library(Rcpp)
+
+sourceCpp('./scripts/getTaxonomyForName.cpp') #~8mins
+#sourceCpp('./scripts/getLocation.cpp') #~8mins
+
 
 pb2_tree <- read.newick('./data/phylo_ml/RAxML_bipartitions.H5_large_pb2')
-ggtree(pb2_tree)
-
-metadata <- read_csv('./data/metadata/h5_metadata.csv')
-will_metadata <- read_csv('./data/metadata/Re_H5_clade2344b_Asia_blast_PB2_meta.will.2.csv')
-
-t <- left_join(metadata, will_metadata )
 tip_labels <- TipLabels(pb2_tree) 
 
 
-joined_data <- tip_labels %>%
-  sapply(., str_split_1, pattern = '\\|') %>%
-  sapply(., `[`, 4) %>%
-  bind_cols() %>%
-  setNames('isolate_id') %>%
-  left_join(metadata)
-
-prob_seqs <- joined_data %>% filter(is.na(isolate_name))
-
-tree <- pb2_tree
-n<-length(tree$tip.label)
-ee<-setNames(tree$edge.length[sapply(4:n,function(x,y)   which(y==x),y=tree$edge[,2])],tree$tip.label)
+#metadata <- read_csv('./data/metadata/h5_metadata.csv')
 
 
-data_from_tipnames <- tip_labels %>% 
-  gsub('\\|$', '', .) %>%
-  sapply(., str_split_1, pattern = '\\|') %>%
-  sapply(., function(x) ifelse(!grepl('\\.', tail(x, n = 4)), return(x), NA)) %>%
-  .[!is.na(.)] %>%
-  do.call(rbind.data.frame,. )
 
 
-#Isolate id = starts with EPI_ISL
-#subtype = HxNx
-#isolate name = 
-#segment
-#date
+
+#joined_data <- tip_labels %>%
+  #sapply(., str_split_1, pattern = '\\|') %>%
+  #sapply(., `[`, 4) %>%
+  #bind_cols() %>%
+  #setNames('isolate_id') %>%
+  #left_join(metadata)
+
+
 countries <- ISOcodes::ISO_3166_2 %>% 
   separate_wider_delim(Code, delim = '-', names = c('Alpha_2', 'subdivision')) %>%
   #left_join(cbind.data.frame(ISOcodes::ISO_3166_1, Type = 'nationsate'), by = join_by(Alpha_2, Type)) %>%
@@ -57,22 +43,19 @@ countries <- ISOcodes::ISO_3166_2 %>%
                                .default = location))
 
 
-birds <- read_csv('ebird_taxonomy_v2023.csv') %>% 
-  rename_with( .fn = ~tolower(.x)) %>%
-  dplyr::select(-c(contains('taxon'), species_code, report_as)) %>%
-  filter(category %in% c('species', 'slash', 'domestic', 'spuh')) %>%
-  mutate(across(contains('name'), .fns = ~ tolower(.x))) %>%
-  mutate(primary_com_name = gsub('gray', 'grey', primary_com_name))
 
 
-remainder <- 
-MyFunc4 <- function(x){
+
+FormatTipData <- function(x){
   
-  isolate.id = x[grep('EPI_ISL_*' ,x)]
-  subtype = x[grep('H[[:digit:]]{1,}N[[:digit:]]*' ,x)]
-  segment = x[grep('HA|PB4|PB2|PA|NP|NA|NS' ,x)]
-  date = x[grep('^[[:digit:]][[:digit:]][[:digit:]][[:digit:]]-[[:digit:]][[:digit:]]-[[:digit:]][[:digit:]]' ,x)]
-  isolate.name = x[grep('/[^/]*/[^/]*/' ,x)]
+  isolate.id <- x[grep('EPI_ISL_*' , x)]
+  
+  subtype <-  x[grep('H[[:digit:]]{1,}N[[:digit:]]*' ,x)] %>%
+    str_extract(., 'H[[:digit:]]{1,}N[[:digit:]]*')
+  
+  date <- x[grep('^[[:digit:]][[:digit:]][[:digit:]][[:digit:]]-[[:digit:]][[:digit:]]-[[:digit:]][[:digit:]]' ,x)]
+  
+  isolate.name <- x[grep('/[^/]*/[^/]*/' ,x)]
   
   if(identical(isolate.id, character(0))){
     isolate.id <- NA
@@ -80,56 +63,63 @@ MyFunc4 <- function(x){
   
   if(identical(subtype, character(0))){
     subtype <- NA
-    print(subtype)
   }
-  if(identical(segment, character(0))){
-    segment <- NA
-  } 
+  
   if(identical(date, character(0))){
     date <- NA
   } 
+  
   if(identical(isolate.name, character(0))){
     isolate.name <- NA
   }
   
-  out <- cbind.data.frame('isolate.id' = isolate.id,
-                    'subtype' = subtype,
-                    'segment' = segment,
-                    'date' = date,
-                    'isolate.name' = isolate.name) #%>%
-  # as_tibble()
+  out <- cbind.data.frame(
+    'isolate.id' = isolate.id,
+    'subtype' = subtype,
+    'date' = date,
+    'isolate.name' = isolate.name) %>%
+    distinct()
   
   
   return(out)
 }
 
-df <- lapply(data_from_tipnames, MyFunc4) %>%
+df <- tip_labels %>% 
+  gsub('\\|$', '', .) %>%
+  sapply(., str_split_1, pattern = '\\|') %>%
+  lapply(., FormatTipData) %>%
   bind_rows() %>%
-  mutate(subtype = str_extract(subtype, 'H[[:digit:]]{1,}N[[:digit:]]*')) %>%
+  as_tibble() %>%
+  
+  # Format Dates
   mutate(date = parsedate::parse_date(date) %>% 
            as.Date()) %>%
   mutate(decimal.date = decimal_date(date)) %>%
+  mutate(week.date  = format(date, '%Y-%V')) %>%
+  
+  # Format isolate name
   mutate(isolate.name = gsub("_(\\d{4})|(\\d{4})_", "\\1",  perl = TRUE, isolate.name)) %>%
   
   mutate(isolate.name = case_when(!grepl( "/(\\d{4})$", isolate.name) & !grepl('/$', isolate.name) ~ paste0(isolate.name, '/NA'),
                                   !grepl( "/(\\d{4})$", isolate.name) & grepl('/$', isolate.name) ~ paste0(isolate.name, 'NA'),
                                   .default = isolate.name)) %>%
- 
+  
+  # Extract data from isolate name
   separate_wider_delim(isolate.name,
                        delim = '/', 
                        names = c('virus_species', 'source', 'location', 'id_unsure', 'year'),
                        cols_remove = F, 
                        too_few = 'align_end',
                        too_many = 'debug') %>%
+  
+  # Drop extraneous cols and format
   dplyr::select(-c(year, contains('isolate.name_'))) %>%
   mutate(virus_species = case_when(str_length(source) == 1  ~ source,
                                    .default = virus_species)) %>%
   mutate(source = case_when(str_length(source) == 1 ~ NA,
                                    .default = source)) %>%
 
-  
-  
-  # Bird names/order
+  # Format source before taxa allocation
   mutate(source =  gsub('_', ' ',tolower(source))) %>%
   mutate(primary_com_name = case_when(grepl('common|eurasian teal|green-winged-teal', source) ~ 'green-winged teal',
                                       grepl('spot-billed duck', source) ~ 'eastern spot-billed duck',
@@ -182,37 +172,54 @@ df <- lapply(data_from_tipnames, MyFunc4) %>%
                                       grepl('\\bpigeon\\b', source) ~ 'pigeon/dove sp.',
                                       grepl('\\bteal\\b', source) ~ 'teal sp.',
                                       
- 
+                                      source %in%  c('aquatic bird', 'avian', 'bird', 'wild bird', 'wild bird ') ~ 'bird sp.',
+                                      source %in% c('en', 'env', 'environment', 'environmental', 'water', 'wild bird feces') ~ 'environment',
+                                      grepl('red fox', source) ~ 'red fox',
+                                      grepl('attus norvegicus', source) ~ 'brown rat',
                                       .default = source)) %>%
+  # Get host taxonomy
+  rowwise() %>%
+  mutate(taxonomy = getTaxonomyForName(primary_com_name)) %>%
+  as_tibble() %>%
+  unnest(taxonomy) %>%
+  rename_with( .fn = ~tolower(.x)) %>%
+  mutate(is.domestic = case_when(grepl('domestic type', sci.name) ~ 'domestic',
+                                 .default = 'wild')) %>%
+  mutate(is.bird = case_when(grepl('ormes$', order) ~ 'bird',
+                                 .default = 'other')) %>%
+  
+
+  
+  
   #rename(primary_com_name = source) %>%
   #mutate(sci_name = primary_com_name) %>%
-  left_join(birds, join_by(primary_com_name)) %>%
-  dplyr::select(-c(virus_species, category, id_unsure)) %>%
+  #left_join(birds, join_by(primary_com_name)) %>%
+  
 
 # family
-  mutate(family = case_when(
-    primary_com_name %in% c('en', 'env', 'environment', 'environmental', 'water', 'wild bird feces') ~ 'environment',
-    primary_com_name %in% c('skunk') ~ 'mephitidae',
-    primary_com_name %in% c('tanuki', 'ezo red fox') ~ 'canidae',
-    primary_com_name %in% c('rattus norvegicus') ~ 'muridae',
-    primary_com_name %in% c('feline') ~ 'felidae',
+  #mutate(family = case_when(
+    #primary_com_name %in% c('en', 'env', 'environment', 'environmental', 'water', 'wild bird feces') ~ 'environment',
+    #primary_com_name %in% c('skunk') ~ 'mephitidae',
+    #primary_com_name %in% c('tanuki', 'ezo red fox') ~ 'canidae',
+    #primary_com_name %in% c('rattus norvegicus') ~ 'muridae',
+    #primary_com_name %in% c('feline') ~ 'felidae',
     
-  .default = family)) %>%
+  #.default = family)) %>%
   
   # order
-  mutate(order = case_when(
-    primary_com_name %in% c('en', 'env', 'environment', 'environmental', 'water', 'wild bird feces') ~ 'environment',
-    primary_com_name %in% c('skunk', 'tanuki', 'ezo red fox', 'feline') ~ 'carnivora',
-    primary_com_name %in% c('rattus norvegicus') ~ ' Rodentia',
-    .default = order)) %>%
+ # mutate(order = case_when(
+   # primary_com_name %in% c('en', 'env', 'environment', 'environmental', 'water', 'wild bird feces') ~ 'environment',
+   # primary_com_name %in% c('skunk', 'tanuki', 'ezo red fox', 'feline') ~ 'carnivora',
+   # primary_com_name %in% c('rattus norvegicus') ~ ' Rodentia',
+    #.default = order)) %>%
   
   # species group
-  mutate(species_group = case_when(
-    primary_com_name %in% c('en', 'env', 'environment', 'environmental', 'water', 'wild bird feces') ~ 'Environment',
-    primary_com_name %in% c('skunk', 'tanuki', 'ezo red fox', 'feline') ~ 'Carnivores',
-    primary_com_name %in% c('rattus norvegicus') ~ 'Rodents',
-    primary_com_name %in% c('aquatic bird', 'avian', 'bird', 'wild bird', 'wild bird ') ~ 'Unknown bird',
-    .default = species_group)) %>%
+  #mutate(species_group = case_when(
+   # primary_com_name %in% c('en', 'env', 'environment', 'environmental', 'water', 'wild bird feces') ~ 'Environment',
+   # primary_com_name %in% c('skunk', 'tanuki', 'ezo red fox', 'feline') ~ 'Carnivores',
+    #primary_com_name %in% c('rattus norvegicus') ~ 'Rodents',
+   # primary_com_name %in% c('aquatic bird', 'avian', 'bird', 'wild bird', 'wild bird ') ~ 'Unknown bird',
+   # .default = species_group)) %>%
   
   # Location (iso name and subdivision)
   mutate(location = tolower(location)) %>%
@@ -279,6 +286,7 @@ df <- lapply(data_from_tipnames, MyFunc4) %>%
   separate_wider_delim(loc_temp, delim = '_', names = c('subdivision','country'), too_few = 'align_end') %>%
   
   # Format columna names
+  dplyr::select(-c(virus_species,  id_unsure)) %>%
   rename(virus.subtype = subtype,
          collection.date = date,
          collection.datedecimal = decimal.date,
@@ -286,9 +294,10 @@ df <- lapply(data_from_tipnames, MyFunc4) %>%
          collection.subdivision = subdivision,
          host.order = order,
          host.family = family,
-         host.sciname = sci_name,
+         host.sciname = sci.name,
          host.commonname = primary_com_name, 
-         host.group = species_group)%>%
+         host.isbird= is.bird,
+         host.isdomestic= is.domestic)%>%
   relocate(virus.subtype,
            isolate.id,
            isolate.name,
@@ -299,8 +308,8 @@ df <- lapply(data_from_tipnames, MyFunc4) %>%
            host.order,
            host.family,
            host.sciname,
-           host.commonname, 
-           host.group) %>%
+           host.isbird,
+           host.isdomestic) %>%
   dplyr::select(-c(type, parent)) 
   
   # Select columns
