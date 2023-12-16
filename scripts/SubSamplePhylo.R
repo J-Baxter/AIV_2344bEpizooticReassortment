d850c01b5a293260e1a52de529c9af1e5892e0d2
James Baxter <james.baxter@ed.ac.uk>
2023-12-16 21:14:44 +0000

    rtt regression plot and initial analysis

--- scripts/SubSamplePhylo.R
+++ scripts/SubSamplePhylo.R
@@ -7,23 +7,47 @@
 
 # Filter phylogenetic tree according to branch length
 FilterBL <- function(phylo,  bl_threshold = 0){
+  require(ape)
   
   return(tiplist)
 }
 
 # filter phylogenetic tree accoding to root-to-tip regression
 FilterR2T <- function(phylo,  sd_threshold = 0){
+  require(ape)
   
   return(tiplist)
 }
 
+# root to tip regression plots
+pb2_tree_dropped <- drop.tip(pb2_tree, TipLabels(pb2_tree)[grep('NA$', TipLabels(pb2_tree))])
+rttdist <- adephylo::distRoot(r2t)
 
+test_df <-  cbind.data.frame('dist' = rttdist, 'tipnames' = names(rttdist)) %>% 
+  left_join(df )
+
+my_model <- lm(dist ~ collection.datedecimal, data =test_df)
+rate = coef(my_model)[2]
+x_intercept <- -coef(my_model)[1] / coef(my_model)[2]
+
+
+ggplot(test_df, aes(x = collection.datedecimal, y = dist)) +
+  geom_point(aes(colour = host.order)) + 
+  scale_y_continuous(expand = c(0,0)) + 
+  scale_x_continuous(expand = c(0,0)) +
+  geom_point(aes(x = x_intercept, y = 0)) + 
+  geom_smooth(method = 'lm', fullrange = T) +
+  coord_cartesian(xlim = c(floor(x_intercept/10) * 10, 2030), ylim = c(0, NA)) +
+  annotate('text', x = x_intercept, y = 0.19, label = paste('x intercept =', round(x_intercept, 2))) +
+  annotate('text', x = x_intercept, y = 0.18, label = paste('rate =', round(rate, 6))) + 
+  theme_classic()
 SubSamplePhylo <- function(phylo, 
                            bl_threshold = 0, 
                            sd_threshold = 2){
   
   require(ape)
-  
+  # If n>2
+  # IF (all lengths to all tips from node x are less than y ) = rmultinom
   if(class(phylo) == 'treedata'){
     phylo = phylo@phylo
     
