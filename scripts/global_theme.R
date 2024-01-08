ebe2a1f40dfa497c4f9e16176cf0ae1d970f9e91
James Baxter <james.baxter@ed.ac.uk>
2024-01-08 00:37:24 +0000

    init global theme

---
+++ scripts/global_theme.R
@@ -0,0 +1,22 @@
+# Set theme
+require(ggplot2)
+require(extrafont)
+
+
+#https://www.fontsquirrel.com/fonts/latin-modern-sans
+font_import(pattern = "lmsans10*") 
+loadfonts()
+
+my_theme <- theme_classic(base_family = "LM Sans 10")+
+  theme(
+    #text = element_text(size=10),
+    axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0), size = 8),
+    axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0), size = 8),
+    axis.text = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0), size = 7),
+    strip.text  = element_text(size = 8),
+    legend.text = element_text(size = 7),
+    legend.title = element_text(size = 8),
+    legend.position = 'none', 
+    panel.spacing = unit(2, "lines"), 
+    strip.background = element_blank()
+  )
