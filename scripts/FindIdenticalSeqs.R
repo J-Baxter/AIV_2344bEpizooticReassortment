91b44b54a35420934a68c87931df4707fc907229
James Baxter <james.baxter@ed.ac.uk>
2024-01-10 22:53:39 +0000

    Function to group identical sequences

---
+++ scripts/FindIdenticalSeqs.R
@@ -0,0 +1,29 @@
+FindIdenticalSeqs<- function(alignment){
+  
+  # calculate pairwise distances
+  distances <- ape::dist.dna(alignment, model = "N")
+  
+  # Identify identical sequences
+  identical_pairs <- which(as.matrix(distances) == 0, arr.ind = TRUE) %>%
+    dplyr::as_tibble(rownames = 'tipnames') %>% 
+    filter(row !=col) 
+  
+  # infer adjcency network 
+  sequence_net <- igraph::graph_from_data_frame(identical_pairs %>% 
+                                                  select(!tipnames), 
+                                                directed = F)
+  
+  # returns a dataframe of tipnames and group number (int)
+  groupings <- merge(identical_pairs,
+                 stack(igraph::clusters(sequence_net)$membership),
+                 by.x = "row", 
+                 by.y = "ind", all.x = TRUE) %>%
+    dplyr::select(c(tipnames, values)) %>%
+    dplyr::distinct() %>%
+    dplyr::rename(group = values)
+  
+  return(groupings)
+}
+
+#identical_seqs <- lapply(subsetted_alignments, FindIdenticalSeqs)
+
