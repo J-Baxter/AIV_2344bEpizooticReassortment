49c855676fbd70ce7d6574041a1e08a7307b23df
James Baxter <39841697+J-Baxter@users.noreply.github.com>
2024-04-07 18:00:09 +0100

    beauti scripts

---
+++ 2024Apr06/plaintreebeauti/mp_asia_relaxLn_Skygrid7-28_1.sh
@@ -0,0 +1,40 @@
+#!/bin/sh 
+################################################################################
+################################################################################
+# This script runs a simple BEAST run on EDDIE 
+# 
+# It requires the beauti XMLs to be present in the current working directory and 
+# for all beast runs to require the same command line options.
+#
+# Works alongside arrayjobsubmit.sh as part of array job submission, but can
+# easily be appropriated for submission of a single BEAST run
+################################################################################
+# Grid Engine options
+#$ -N mp_asia_relaxLn_Skygrid7-28_1.xml
+#$ -cwd
+#$ -pe sharedmem 2
+#$ -l h_vmem=8G
+#$ -l h_rt=167:00:00
+#$ -M james.baxter@ed.ac.uk
+#$ -P roslin_eeid_aiv
+#$ -m baes
+. /etc/profile.d/modules.sh
+################################################################################
+# load BEAGLE and BEAST
+module load roslin/beast/1.10.4-beagle2
+################################################################################
+# Run the program
+echo '=============================================='
+echo '** Hello BEAST user !**'
+echo "This job is running on $HOSTNAME"
+echo 'Start BEAST with AIV data this is a 167 hour run'
+beast mp_asia_relaxLn_Skygrid7-28_1.xml 
+echo '** Done **'
+echo "============================================="
+################################################################################
+################################################################################
+# END #
+################################################################################
+################################################################################
+You can write whatever you want in it.
+This is just an example.
