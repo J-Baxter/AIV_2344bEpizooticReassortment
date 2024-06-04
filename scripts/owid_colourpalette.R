742d2fc887bad3a1cbc4bf24a9b5bd05b62c3605
James Baxter <39841697+J-Baxter@users.noreply.github.com>
2024-06-04 19:59:51 +0100

    init owid palettes

---
+++ scripts/owid_colourpalette.R
@@ -0,0 +1,14 @@
+owid_palette <- c(
+  "BlueGrey" = "#57677D",
+  "RedOrange" = "#CB480E",
+  "Teal" = "#019B94",
+  "Purple" = "#8D4DA2",
+  "Pink" = "#EC0175",
+  "Grey" = "#737373",
+  "Green" = "#019B5D",
+  "Maroony" = "#A54551",
+  "Blue" = "#0184A9",
+  "PastelPink" = "#DC5E78"
+)
+
+show_col(owid_palette)
