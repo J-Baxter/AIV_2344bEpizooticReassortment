fbc6ea5d0890e0be50a0857f45e8fd664b89c6c4
James Baxter <39841697+J-Baxter@users.noreply.github.com>
2024-09-23 00:44:31 +0100

    tidy kmeans workflow. requires annotation,refactoring and cross validation/variable importance testing

--- scripts/clustering_model.R
+++ scripts/clustering_model.R
@@ -24,17 +24,50 @@ library(bayesplot)
 
 ########################################### IMPORT DATA ############################################
 
-combined_data <- read_csv()
-
+combined_data <- read_csv('./2024Aug18/treedata_extractions/2024-09-20_combined_data.csv')
+summary_data <- read_csv('./2024Aug18/treedata_extractions/summary_reassortant_metadata_20240904.csv') %>%
+  select(-c(cluster_label,
+            clade)) 
+  
 
 ########################################### FORMAT DATA ############################################
+kclust_nophylo_data <- summary_data %>%
+  
+  select(-c(contains('range'),
+            Kcluster, 
+            if_mammal,
+            check,
+            group,
+            starts_with('col'))) %>% 
+  
+  # All numeric NA as 0
+  mutate(across(where(is.double), .fns = ~ replace_na(.x, 0))) %>%
+  
+  # Start tidymodels recipe
+  recipe(~ .) %>% 
+  
+  # normalise numeric vectors
+  step_normalize(all_numeric()) %>% 
+  
+  # create dummy variables for categorical predictor
+  step_dummy(Continent_of_Earliest_Date, one_hot = TRUE) %>%
+  step_dummy(Host_of_Earliest_Sample_Date, one_hot = TRUE) %>%
+  
+  # run tidymodels recipe
+  prep() %>%
+  bake(NULL) %>%
+  
+  # exclude columns not to be used
+  #select(-c(cluster_profile, original_diff_coeff, starts_with('median'))) %>%
+  drop_na()
+  
 
-########################################### FORMAT DATA ############################################
 
-model_data <- combined_data %>%
+kclust_updated_data <- combined_data %>%
   
   # select variables of interes
   select(
+    segment,
     cluster_profile,
     group2,
     weighted_diff_coeff,
@@ -45,112 +78,180 @@ model_data <- combined_data %>%
     host_simplifiedhost,
     count_cross_species,
     starts_with('median'),
-    starts_with('max')) %>%
+    starts_with('max')
+    ) %>%
+  
   
   # Substitute NA values in diffusion coefficient with 0
-  replace_na(list(original_diff_coeff = 0,
-                  weighted_diff_coeff = 0,
-                  `median_galliformes-domestic` = 0,
-                  `median_other-bird` = 0,
-                  `median_anseriformes-wild` = 0,
-                  `median_galliformes-wild` = 0,
-                  median_environment = 0,
-                  median_mammal = 0,
-                  `median_charadriiformes-wild` = 0,
-                  median_human = 0,
-                  `median_anseriformes-domestic` = 0)) %>%
-  rename_with(~gsub('-', '_', .x))
+  mutate(across(where(is.double), .fns = ~ replace_na(.x, 0))) %>%
+  rename_with(~gsub('-', '_', .x)) %>%
   
+  mutate(collection_regionname = case_when(grepl('europe', collection_regionname) ~ 'europe',
+                                           grepl('africa', collection_regionname) ~ 'africa',
+                                           grepl('asia', collection_regionname) ~ 'asia',
+                                           grepl('(central|northern) america', collection_regionname) ~ 'central & northern america',
+                                           .default = collection_regionname
+  )) %>%
+  filter(!grepl('\\+', host_simplifiedhost)) %>%
   
-  ################################### INITIAL EXPLORATORY PLOTS #####################################
-# Plot logged data to show zero/non-zero segregation
-
-model_data %>%
-  ggplot() +
-  geom_histogram(aes(x = log1p(weighted_diff_coeff), fill = weighted_diff_coeff>0)) +
-  scale_fill_brewer(palette = 'Dark2', 'Is Zero') +
-  scale_x_continuous('Weighted Diffusion Coefficient') +
-  theme_minimal()
-
-
-# OLS model of 
-
-
+  # Model pre-processing
+  group_by(segment, cluster_profile) %>%
+  slice_sample(n=1) %>%
+  ungroup() %>%
+  
+  # start tidymodels
+  recipe(~ .) %>% 
+  
+  # normalise numeric vectors
+  step_normalize(all_numeric()) %>% 
+  
+  # create dummy variables for categorical predictors
+  step_dummy(collection_regionname, one_hot = TRUE) %>%
+  step_dummy(host_simplifiedhost, one_hot = TRUE) %>%
+  step_dummy(segment, one_hot = TRUE) %>%
+  
+  # run tidymodels recipe
+  prep() %>%
+  bake(NULL) %>%
+  
+  # exclude columns not to be used
+  left_join(kclust_nophylo_data %>% select(-starts_with('group')), by = join_by(cluster_profile)) %>%
+  select(-c(cluster_profile, original_diff_coeff, starts_with('median'))) %>%
+  drop_na() 
 
 
 ####################################### START KCLUST PIPELINE ########################################
-
-model_data_nolabel <- model_data %>%
-  select(-c(group2,
-            cluster_profile))
-
-kclusts <- tibble(k = 1:9) %>%
+kclust_nophylo <- tibble(k = 1:20) %>%
   mutate(
-    kclust = map(l, ~kmeans(model_data, .x)),
+    kclust = map(k, ~kmeans(kclust_nophylo_data %>% select(-c(group2, cluster_profile)), .x)),
     tidied = map(kclust, tidy),
     glanced = map(kclust, glance),
-    augmented = map(kclust, augment, model_data)
+    augmented = map(kclust, augment, kclust_nophylo_data)
   )
 
-# Set MCMC Options
-CHAINS <- 4
-CORES <- 4
-ITER <- 4000
-BURNIN <- ITER/10 # Discard 10% burn in from each chain
-SEED <- 4472
+clusters_nophylo <- 
+  kclust_nophylo %>%
+  unnest(cols = c(tidied))
+
+assignments_nophylo <- 
+  kclust_nophylo %>% 
+  unnest(cols = c(augmented))
+
+clusterings_nophylo <- 
+  kclust_nophylo %>%
+  unnest(cols = c(glanced))
+
+clustersnophylo_plot <- assignments_nophylo %>%
+  filter(k ==3) %>%
+  select(-c(kclust, tidied, glanced)) %>%
+  recipe(~ .) %>%
+  step_pca(all_numeric(), -k, num_comp = 2) %>%  # Perform PCA (e.g., 2 components)
+  prep() %>%
+  bake(NULL)%>%
+  ggplot(., aes(x = PC1, y = PC2)) +
+  geom_point(aes(colour = .cluster, shape = group2), alpha = 0.8) + 
+  scale_shape('Pre-Assigned Classification') + 
+  scale_color_discrete('K-Means Clusters') +
+  theme_minimal() +
+  theme(legend.position = 'bottom',
+        legend.box = "vertical") +               # Stack legends vertically
+  guides(
+    color = guide_legend(order = 1),             # Ensure color legend is first
+    shape = guide_legend(order = 2)              # Shape legend is second
+  )
+
+ggplot(clusterings_nophylo, aes(k, tot.withinss)) +
+  geom_line() +
+  geom_point() +
+  theme_minimal() +
+  scale_y_continuous('Total within-cluster sum of squares') +
+  scale_x_continuous('K', breaks = seq(1,20, by = 1))
 
 
-# Prior Predictive Checks 
-diffusionmodel1_priorpredictive <- brm(
-  bf(weighted_diff_coeff ~ collection_regionname,
-     hu ~ 1),
-  data = model_data,
-  family = hurdle_lognormal(),
-  sample_prior = "yes",
-  chains = CHAINS,
-  cores = CORES, 
-  iter = ITER,
-  warmup = BURNIN,
-  seed = SEED#,
-  #opencl = opencl(c(0, 0)) # Enables GPU accelerated computation, remove if not applicable
-)
+########
+
+kclusts <- tibble(k = 1:20) %>%
+  mutate(
+    kclust = map(k, ~kmeans(kclust_updated_data %>% select(-group2), .x)),
+    tidied = map(kclust, tidy),
+    glanced = map(kclust, glance),
+    augmented = map(kclust, augment, kclust_updated_data)
+  )
+
+clusters <- 
+  kclusts %>%
+  unnest(cols = c(tidied))
+
+assignments <- 
+  kclusts %>% 
+  unnest(cols = c(augmented))
+
+clusterings <- 
+  kclusts %>%
+  unnest(cols = c(glanced))
+
+assignments %>%
+  filter(k %in% seq(from = 3, to = 6)) %>%
+  select(-c(kclust, tidied, glanced)) %>%
+  group_split(k, .keep = TRUE) %>%
+  map(function(df) {
+    # 2. Apply PCA to each group separately
+    df %>%
+      recipe(~ .) %>%
+      step_pca(all_numeric(), -k, num_comp = 2) %>%  # Perform PCA (e.g., 2 components)
+      prep() %>%
+      bake(NULL)
+  }) %>%
+  bind_rows() %>%
+  ggplot(., aes(x = PC1, y = PC2)) +
+  geom_point(aes(colour = .cluster, shape = group2), alpha = 0.8) + 
+  theme_minimal() +
+  facet_wrap(~ k)
+
+clustersphylo_plot <- assignments %>%
+  filter(k ==3) %>%
+  select(-c(kclust, tidied, glanced)) %>%
+  recipe(~ .) %>%
+  step_pca(all_numeric(), -k, num_comp = 2) %>%  # Perform PCA (e.g., 2 components)
+  prep() %>%
+  bake(NULL)%>%
+  ggplot(., aes(x = PC1, y = PC2)) +
+  geom_point(aes(colour = .cluster, shape = group2), alpha = 0.8) + 
+  scale_shape('Pre-Assigned Classification') + 
+  scale_color_discrete('K-Means Clusters') +
+  theme_minimal() +
+  theme(legend.position = 'bottom') +               # Stack legends vertically
+  guides(
+    color = guide_legend(order = 1),             # Ensure color legend is first
+    shape = guide_legend(order = 2)              # Shape legend is second
+  )
 
 
-diffusionmodel1_priorpreds <- posterior_predict(diffusionmodel1_priorpredictive)
-n <- sample(1:nrow(diffusionmodel1_priorpreds), 50)
 
-color_scheme_set("green")
-ppc_dens_overlay(y = log1p(model_data$weighted_diff_coeff),
-                 yrep = log1p(diffusionmodel1_priorpreds[1:10,]))
+assignments %>%
+  filter(k == 3) %>%
+  select(.cluster, group2) %>%
+  summarise(n = n(), .by = c(.cluster, group2)) %>%
+  pivot_wider(names_from = 'group2', values_from = 'n')
 
-
-# Fit Model
-diffusionmodel1_fit <- brm(
-  bf(weighted_diff_coeff ~ collection_regionname,
-     hu ~ 1),
-  data = model_data,
-  family = hurdle_lognormal(),
-  chains = CHAINS,
-  cores = CORES, 
-  iter = ITER,
-  warmup = BURNIN,
-  seed = SEED#,
-  #opencl = opencl(c(0, 0)) # Enables GPU accelerated computation, remove if not applicable
-)
+# Elbow plot
+ggplot(clusterings, aes(k, tot.withinss)) +
+  geom_line() +
+  geom_point() +
+  theme_minimal() +
+  scale_y_continuous('Total within-cluster sum of squares') +
+  scale_x_continuous('K', breaks = seq(1,20, by = 1))
 
 
-# Posterior Predictive Checks
-diffusionmodel1_posteriorpreds <- posterior_predict(diffusionmodel1_fit)
-n <- sample(1:nrow(diffusionmodel1_posteriorpreds), 50)
+##### before/after plots
 
-color_scheme_set("green")
-ppc_dens_overlay(y = log1p(model_data$weighted_diff_coeff),
-                 yrep = log1p(diffusionmodel1_posteriorpreds[1:10,]))
-
-
-# Extract Model Terms and Marginal/Conditional Effects
-diffusionmodel1_fit_tidy <- tidy(model_hurdle)
-
+library(cowplot)
+legend <- get_legend(clustersphylo_plot)
+figure_upper <- plot_grid(clustersnophylo_plot + theme(legend.position = 'none'),
+                          clustersphylo_plot + theme(legend.position = 'none'), 
+                          align = 'hv',
+                          labels = 'AUTO')
+figure <- plot_grid(figure_upper, legend , nrow = 2, rel_heights = c(0.9,0.1))
 
 ####################################################################################################
 ####################################################################################################
