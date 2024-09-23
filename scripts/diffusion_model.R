3c6dce9bb4c552d4f2597abefe4cc67fff26c178
James Baxter <james.baxter@ed.ac.uk>
2024-09-23 13:39:58 +0100

    refactored and deprecated code deleted. introduced pairwise comparison for season effect on hurdle component

--- scripts/diffusion_model.R
+++ scripts/diffusion_model.R
@@ -20,7 +20,9 @@ library(broom)
 library(broom.mixed)
 library(tidybayes)
 library(bayesplot)
-
+library(emmeans)
+library(marginaleffects)
+library(magrittr)
 
 ########################################### IMPORT DATA ############################################
 combined_data <- read_csv('./2024Aug18/treedata_extractions/2024-09-20_combined_data.csv')
@@ -66,7 +68,7 @@ diffusion_data <- combined_data %>%
             by = join_by(cluster_profile)) %>%
 
   # season (breeding, migrating_north, migrating_south, overwintering)
-  mutate(collection_month = date_decimal(TMRCA) %>% format(., "%m"),
+  mutate(collection_month = date_decimal(TMRCA) %>% format(., "%m") %>% as.integer(),
          season = case_when(collection_month %in% c(12,1,2) ~ 'overwintering', 
                             collection_month %in% c(3,4,5)  ~ 'migrating_north', 
                             collection_month %in% c(6,7,8)  ~ 'breeding', 
@@ -87,14 +89,23 @@ model_data %>%
 
 # OLS model of 
 
+######################################## DEFINE FORMULA ############################################
+# We assume a hurdle lognormal model, in which the hurdle process is determined the season in which 
+# the TMRCA is estimated to be, and the lognormal component is determined by host richness, persistence
+# in anseriformes and charadriiformes. Both model components are conditional on segment from which 
+# the measurement is taken and region of origin
 
-
-
-####################################### START BRMS PIPELINE ########################################
+diffusion_formula <- bf(weighted_diff_coeff ~ 1 + host_richness + median_anseriformes_wild +  median_charadriiformes_wild + 
+                          (1|segment + collection_regionname),
+                        hu ~ 1 +  season + (1|segment + collection_regionname))
+####################################### DEFINE PRIORS ########################################
 
 # Set Priors
 diffusionmodel1_priors <- c()
 
+
+
+####################################### SET MCMC OPTION ########################################
 
 # Set MCMC Options
 CHAINS <- 4
@@ -104,11 +115,12 @@ BURNIN <- ITER/10 # Discard 10% burn in from each chain
 SEED <- 4472
 
 
+###################################### PRIOR PREDICTIVE SIM ########################################
+
 # Prior Predictive Checks 
-# what does weighted diffusion coefficient measure specifically (ie speed vs geographical distance)
+
 diffusionmodel1_prior <- brm(
-  bf(weighted_diff_coeff ~ 1 + host_richness + median_anseriformes_wild + median_charadriiformes_wild + (1|segment + collection_regionname),
-     hu ~ 1 + host_simplifiedhost + season + (1|segment + collection_regionname)),
+  ,
   data = diffusion_data,
   family = hurdle_lognormal(),
   sample_prior = "yes",
@@ -121,7 +133,7 @@ diffusionmodel1_prior <- brm(
   )
 
 
-
+# PRIOR PREDICTIVE CHECKS
 color_scheme_set("green")
 plot_priorpredictive <- pp_check(diffusionmodel1_priorpredictive, ndraws = 100) + 
   theme_minimal()
@@ -133,8 +145,7 @@ plot_priorpredictive
 
 
 
-
-# Fit Model
+############################################ FIT MODEL #############################################
 diffusionmodel1_fit <- brm(
   bf(weighted_diff_coeff ~ 1 + host_richness + median_anseriformes_wild + median_charadriiformes_wild + (1|segment + collection_regionname),
      hu ~ 1 + season + (1|segment + collection_regionname)),
@@ -142,12 +153,16 @@ diffusionmodel1_fit <- brm(
   family = hurdle_lognormal(),
   chains = CHAINS,
   cores = CORES, 
+  threads = 2, 
   iter = ITER,
   warmup = BURNIN,
   seed = SEED,
   control = list(adapt_delta = 0.99))
 
+# CONVERGENCE CHECK 
 
+
+# POSTERIOR PREDICTIVE CHECKS
 color_scheme_set('red')
 plot_posteriorpredictive <- pp_check(diffusionmodel1_fit, ndraws = 100) + 
   theme_minimal()
@@ -156,19 +171,116 @@ plot_posteriorpredictive$data %<>%
   mutate(value = log1p(value))
 
 plot_posteriorpredictive
-# Conditional effect of host mrca on hurdle
-# predicted probability of diffusion coefficient == 0
+
+
+##################################### PREDICTIONS AND EFFECTS ######################################
+
+
+# predicted probability of diffusion coefficient == 0 per season
+condition_data <- plot(conditional_effects(diffusionmodel1_fit, dpar = "hu"),
+                       plot = FALSE,
+                       re_formula = NULL)[[4]]$data
+
+
+# Can we compare across random effect region? <<<<<<<< OUTSTANDING TASK
+
+
+# Marginal effects of host richness, and host persistence on dispersal velocity (mu part of model)
+# does not determine = 0 / !=0 process
+
+# This represents the instantaneous slopes at each of these values of host richness in 'mu' part of
+# the model.
+# e.g at host richness = 3, an +1 increase in host richness is associated with an additional 19597 of
+# diffusion coefficient
+hostrichness_marginal <-  diffusionmodel1_fit %>%
+  emtrends(~ host_richness,
+           var = "host_richness", 
+           dpar = "mu",
+           at = list(host_richness = seq(1, 5, 1)))
+
+# at response scale
+hostrichness_marginal_response <-  diffusionmodel1_fit %>%
+  emtrends(~ host_richness,
+           var = "host_richness", 
+           dpar = "mu",
+           type = 'response',
+           tran ='log',
+           regrid = 'response',
+           at = list(host_richness = seq(1, 5, 1)))
+
+
+# This represents the instantaneous slopes at each of these values of charadriiformeswild in 'mu' part of
+# the model.
+charadriiformeswild_marginal <-  diffusionmodel1_fit %>%
+  emtrends(~ median_charadriiformes_wild,
+           var = "median_charadriiformes_wild", 
+           dpar = "mu",
+           at = list(median_charadriiformes_wild = seq(1, 5, 1)))
+
+# at response scale
+charadriiformeswild_marginal_response <-  diffusionmodel1_fit %>%
+  emtrends(~ median_charadriiformes_wild,
+           var = "median_charadriiformes_wild", 
+           dpar = "mu",
+           type = 'response',
+           tran ='log',
+           regrid = 'response',
+           at = list(median_charadriiformes_wild = seq(1, 5, 1)))
+
+
+# This represents the instantaneous slopes at each of these values of anseriformeswild in 'mu' part of
+# the model.
+anseriformeswild_marginal<-  diffusionmodel1_fit %>%
+  emtrends(~ median_anseriformes_wild,
+           var = "median_anseriformes_wild", 
+           dpar = "mu",
+           at = list(median_anseriformes_wild = seq(1, 5, 1)))
+
+# at response scale
+anseriformeswild_marginal_response <-  diffusionmodel1_fit %>%
+  emtrends(~ median_anseriformes_wild,
+           var = "median_anseriformes_wild", 
+           dpar = "mu",
+           type = 'response',
+           tran ='log',
+           regrid = 'response',
+           at = list(median_anseriformes_wild = seq(1, 5, 1)))
+
+
+##################################### PAIRWISE COMPARISONS ######################################
+
+# pair wise comparisons of season on probability of diffusion coefficient == 0
+diffusionmodel1_fit %>%
+  emmeans(~season,
+          var = 'season',
+          dpar = 'hu')%>%
+  contrast(method = "revpairwise",
+           ref = 'migrating_south', 
+           type = 'response')
+
+############################################### PLOTS ############################################
+
+# Conditional effect of MRCA season on the probability that a reassortant is observed to spread at all
+# (ie hurdle component)
 plot(conditional_effects(diffusionmodel1_fit, dpar = "hu"), plot = FALSE)[[4]]$data %>%
   ggplot(aes(x = effect1__, y = estimate__)) +
   geom_point(size = 3) +
-  geom_linerange(aes(ymin = lower__, ymax = upper__, x = effect1__)) +
-  scale_y_continuous('Predicted Probability Of Diffusion Coefficient == 0') + 
-  theme_minimal() 
-  
-cond <- plot(conditional_effects(diffusionmodel1_fit, dpar = "mu"), plot = FALSE)[-4] %>%
+  geom_linerange(aes(ymin = 1-lower__, ymax = 1-upper__, x = effect1__)) +
+  scale_y_continuous('Predicted probability that diffusion coefficient > 0') + 
+  scale_x_discrete('Season of MRCA',
+                   labels = c('breeding' = 'Breeding',
+                              'migrating_north' = 'Spring Migration',
+                              'migrating_south' = 'Autumn Migration',
+                              'overwintering' = 'Overwintering')) +
+  theme_minimal(base_size = 18) 
+
+
+# Conditional effect of median persistence time (anser and charadriformes) and host richness on
+# the diffusion coefficient
+plot(conditional_effects(diffusionmodel1_fit, dpar = "mu"), plot = FALSE)[-4] %>%
   lapply(., function(x)x$data) %>%
   bind_rows(, .id = 'var') %>%
-  filter(var != 'median_anseriformes_wild')  %>%
+  #filter(var != 'median_anseriformes_wild')  %>%
   ggplot(.,aes(x = effect1__, y = estimate__, colour = var)) +
   geom_line() +
   geom_ribbon(aes(ymin = lower__ , ymax = upper__,, x = effect1__, fill = var), colour = NA, alpha = 0.2) + 
@@ -176,117 +288,6 @@ cond <- plot(conditional_effects(diffusionmodel1_fit, dpar = "mu"), plot = FALSE
   theme_minimal() +
   coord_cartesian(xlim = c(0,5)) +
   theme(legend.position = 'bottom')
-
-diffusionmodel1_fit |> emtrends(
-  ~host_richness,
-  var = 'host_richness',
-  regrid = "response",  
-  tran = 'log',
-  type = "response",
-  at = list(host_richness = seq(1, 5, 1)),
-  dpar = "mu") |> 
-  gather_emmeans_draws() |> 
-  ggplot(aes(x = host_richness, y = .value)) +
-  stat_lineribbon(size = 1)
-
-hurdle_intercept <- tidy(diffusionmodel1_fit) |> 
-  filter(term == "hu_(Intercept)") |> 
-  pull(estimate)
-
-hurdle_season <- tidy(diffusionmodel1_fit) |> 
-  filter(term == "hu_seasonoverwintering") |> 
-  pull(estimate)
-
-plogis(hurdle_intercept + hurdle_season) - plogis(hurdle_intercept)
-
-
-diffusionmodel1_fit |> 
-  emtrends(~ host_richness, var = "median_charadriiformes_wild", dpar = "mu",
-           at = list(host_richness = seq(1, 5, 1)))
-
-
-
-test <- diffusionmodel1_fit %>% 
-  epred_draws(newdata = expand_grid(host_richness = 0,
-                                    median_anseriformes_wild = 0,
-                                    median_charadriiformes_wild = 0,
-                                    season = c('overwintering'),
-                                    region = unique(diffusion_data$collection_regionname)[-5]), 
-            re_formula = ~ (1|region))  # or re_formula = ~ (1 | region)
-
-test <- diffusionmodel1_fit %>% 
-  epred_draws(newdata = expand_grid(host_richness = seq(0,6, by = 1),
-                                    median_anseriformes_wild = 0,
-                                    median_charadriiformes_wild = 0,
-                                    season = c('overwintering'),
-                                    region = unique(diffusion_data$collection_regionname)[-5]), 
-              re_formula =~ (1|region))
-
-ggplot(test, 
-       aes(x = host_richness, y = .epred)) +
-  stat_lineribbon() +
-  scale_fill_brewer(palette = "Reds") +
-  labs(x = "Host Richness", y = "Predicted Diffusion Coefficient",
-       fill = "Credible interval") +
-  facet_wrap(vars(region))
-
-ggplot(test, 
-       aes(x = .epred, 
-           fill = region)) +
-  stat_halfeye() + 
-  scale_x_continuous(trans = 'log')
-  coord_cartesian(xlim = c(0, 100000))
-
-diffusionmodel1_fit %>%
-  predicted_draws(newdata = tibble(collection_regionname = ,
-                                   segment = 'ha'))
-  emtrends(~ lifeExp, var = "lifeExp", dpar = "mu",
-           at = list(lifeExp = seq(30, 80, 10)))
-
-
-diffusionmodel1_fit %>% 
-  emmeans(~ collection_regionname,
-          epred = TRUE,  dpar = "hu") %>% 
-  contrast(method = "revpairwise")
-
-# Conditional effect of time in charadrifformes, and anseriformes on dispersal velocity
-diffusionmodel1_fit |> 
-  emtrends(~  median_anseriformes_wild,
-           var = " median_anseriformes_wild",
-           dpar = "mu", 
-           regrid = "response",
-           tran = "log", 
-           type = "response",
-           at = list( median_anseriformes_wild = seq(0, 10, 2)))
-
-
-diffusionmodel1_fit|> marginaleffects::comparisons(
-  newdata = datagrid( median_anseriformes_wild = seq(0, 10, 2)),
-  dpar = "mu", 
-  transform_pre = "expdydx"
-)
-
-model_gdp_hurdle_life %>%
-  emmeans(~ lifeExp, var = "lifeExp", epred = TRUE,
-          at = list(lifeExp = seq(30, 80, 1))) |> 
-  gather_emmeans_draws() |> 
-  ggplot(aes(x = lifeExp, y = .value)) +
-  stat_lineribbon(size = 1, color = clrs[4]) +
-  scale_fill_manual(values = colorspace::lighten(clrs[4], c(0.95, 0.7, 0.4))) +
-  scale_y_continuous(labels = label_dollar()) +
-  labs(x = "Life expectancy", y = "Predicted GDP per capita",
-       subtitle = "Expected values from mu and hu parts (epred = TRUE)",
-       fill = "Credible interval")
-
-t <- plot(conditional_effects(diffusionmodel1_fit, dpar = "mu"), plot = FALSE)[[4]]$data %>% median_anseriformes_wild
-  ggplot(aes(x = effect1__, y = estimate__)) +
-  geom_ribbon(aes(ymin = lower__, ymax = upper__), alpha = 0.3, color = NA) +
-  geom_line(size = 1, color = clrs[5]) +
-  scale_y_continuous(labels = label_percent()) +
-  labs(x = "Life expectancy", y = "Predicted probability\nof seeing $0 GDP per capita",
-       subtitle = "Hurdle part of the model (dpar = \"hu\")") +
-  theme_nice()
-
 
 
 ####################################################################################################
