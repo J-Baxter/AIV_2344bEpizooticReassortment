ed21a3524f9f41ca461f14eba791211b11686480
James Baxter <39841697+J-Baxter@users.noreply.github.com>
2024-09-23 09:51:34 +0100

    init entire workflow

--- scripts/numberofreassortants_model.R
+++ scripts/numberofreassortants_model.R
@@ -26,7 +26,41 @@ combined_data <- read_csv()
 
 ########################################### FORMAT DATA ############################################
 
+count_data <- combined_data %>%
+  filter(segment == 'ha') %>%
+  mutate(collection_monthyear = date_decimal(TMRCA) %>% format(., "%Y-%m")) %>%
+  mutate(collection_regionname = case_when(grepl('europe', collection_regionname) ~ 'europe',
+                                           grepl('africa', collection_regionname) ~ 'africa',
+                                           grepl('asia', collection_regionname) ~ 'asia',
+                                           grepl('(central|northern) america', collection_regionname) ~ 'central & northern america',
+                                           .default = collection_regionname
+  )) %>%
+  filter(!is.na(collection_regionname)) %>%
+  select(collection_regionname, collection_monthyear, cluster_profile, group2) %>% # include all month-years over collection period to generate zer countr
+  summarise(n_reassortants = n_distinct(cluster_profile), 
+            .by = c(collection_monthyear, collection_regionname, group2)) %>%
+  mutate(collection_monthyear = ym(collection_monthyear)) %>%
+  group_by(collection_regionname, group2) %>%
+  complete(collection_monthyear = seq(min(collection_monthyear), max(collection_monthyear), by = "month")) %>% 
+  replace_na(list(n_reassortants = 0)) %>%
+  ungroup() %>%
+  mutate(collection_year = collection_monthyear %>% format(., "%Y") %>% as.integer()) %>%
+  summarise(n_reassortants = sum(n_reassortants), .by = c(collection_year, collection_regionname , group2)) %>%
+  arrange(collection_year) 
+  
 
+
+ggplot(grouped_lines) +
+  geom_bar(aes(x = collection_monthyear, y = n_reassortants, fill = group2), stat = 'identity') + 
+  facet_grid(rows = vars(collection_regionname),  labeller = labeller(collection_regionname = label_wrap_gen(10))) +
+  scale_y_continuous(breaks = seq(0,14, by = 2), 'Number of Reassortants')+
+  scale_x_date('Resassortant MRCA') +
+  scale_fill_manual(
+    'Reassortant Class',
+    values = riskgroup_colour %>% pull(Trait, name = group2)) +
+    #labels = riskgroup_colour %>% pull(group2)) +
+  theme_minimal()  +
+  theme(legend.position = 'bottom')
 ####################################### START BRMS PIPELINE ########################################
 
 # Set Priors
@@ -42,26 +76,136 @@ SEED <- 4472
 
 
 # Prior Predictive Checks 
-diffusionmodel1_priorpredictive <- brm(
-  n_reassortants ~ n_sequences + collection.region.name,
-  data = grouped_lines,
-  family = negbinomial(), sample_prior = "yes",
+formula_y1 <- bf(n_reassortants ~ collection_regionname, family = zero_inflated_negbinomial())
+formula_y2 <- bf(group2 ~ collection_regionname, family = categorical())
+
+# Multivariate model - no correlation
+multivar_model <- brm(
+  formula_y1 + formula_y2,  # Joint model formula
+  data = grouped_lines,           # Dataframe containing y1, y2, x1, x2
   chains = CHAINS,
   cores = CORES, 
   iter = ITER,
   warmup = BURNIN,
-  seed = SEED#,
-  #opencl = opencl(c(0, 0)) # Enables GPU accelerated computation, remove if not applicable
+  seed = SEED,
+  sample_prior = "yes",
+  control = list(adapt_delta = 0.95)
 )
 
 
-diffusionmodel1_priorpreds <- posterior_predict(diffusionmodel1_priorpredictive)
-n <- sample(1:nrow(diffusionmodel1_priorpreds), 50)
+fit_year <- brm(
+  mvbind(n_reassortants, group2) ~ 1 + collection_regionname + (1|collection_year),  # Multivariate outcome
+  family = list(zero_inflated_negbinomial(), categorical()),  # Count and class families
+  data = count_data,           # Dataframe containing y1, y2, x1, x2
+  chains = CHAINS,
+  cores = CORES, 
+  iter = ITER,
+  warmup = BURNIN,
+  seed = SEED,
+  control = list(adapt_delta = 0.95)
+)
+
+
+
+# Marginal probability density of the number of unique reassortants/region
+# ie, irrespective of class
+count_prob <- fit_year %>% 
+  epred_draws(newdata = count_data %>%
+                select(collection_regionname) %>%
+                distinct(),resp = "nreassortants",
+              re_formula = NA)
+
+count_prob %>% 
+  median_hdi(.epred)
+
+ggplot(count_prob, aes(x = .epred, fill = collection_regionname , y = collection_regionname)) +
+  stat_halfeye() +
+  theme_minimal() + 
+  scale_x_continuous('Number of Unique Reassortants/Year') +
+  
+  # Scales
+  scale_y_discrete('Region of Origin', labels = function(x) str_wrap(x, width = 20) %>% str_to_title())+
+  scale_colour_manual(
+    'Reassortant Class',
+    values = region_colour %>% pull(Trait, name = Name))  +
+  theme(legend.position = 'none') + 
+  coord_cartesian(xlim = c(0,10))
+
+# Marginal probability of reassortant class/region
+# ie, irrespective of number
+class_prob <- fit_year %>% 
+  epred_draws(newdata = count_data %>%
+                select(collection_regionname) %>%
+                distinct(),resp = "group2",
+              re_formula = NA)
+class_prob %>% 
+  median_hdi(.epred)
+
+ggplot(class_prob, aes(x = collection_regionname, colour = .category, y = .epred)) +
+  geom_boxplot() +
+  theme_minimal() + 
+  scale_y_continuous('Posterior Probability of Reassortant Class', labels = scales::percent) +
+  
+  # Scales
+  scale_x_discrete('Region of Origin', labels = function(x) str_wrap(x, width = 20) %>% str_to_title())+
+  scale_colour_manual(
+    'Reassortant Class',
+    values = riskgroup_colour %>% pull(Trait, name = group2)) +
+  theme(legend.position = 'bottom')
+
+
+#### Contrasts ####
+fit_year %>% 
+  emmeans(., ~ collection_regionname ,
+          epred = TRUE,
+          resp = 'group2') %>% 
+  filter(rep.meas == 'nreassortants') %>%
+  contrast(method = 'revpairwise')
+
+
+
+joint_prob <- count_prob %>%
+  left_join(class_prob, 
+            by = c(".draw",  '.row', "collection_regionname"), 
+            suffix = c("_count", "_class")) %>%
+  mutate(joint_epred = .epred_count * .epred_class)
+
+
+
+
+
+numberreassortants_priorpredictive <- brm(
+  n_reassortants|trunc(lb = 1) ~ 1 + collection_regionname,
+  data = grouped_lines,
+  family = negbinomial(), 
+  chains = CHAINS,
+  cores = CORES, 
+  iter = ITER,
+  warmup = BURNIN,
+  seed = SEED,
+  sample_prior = "yes",
+  control = list(adapt_delta = 0.95)
+)
 
 color_scheme_set("green")
-ppc_dens_overlay(y = log1p(model_data$weighted_diff_coeff),
-                 yrep = log1p(diffusionmodel1_priorpreds[1:10,]))
+plot_priorpredictive <- pp_check(fit_year, ndraws = 100) + 
+  theme_minimal()
 
+plot_priorpredictive$data %<>%
+  mutate(value = log1p(value))
+
+plot_priorpredictive
+
+color_scheme_set("red")
+fit_priorpredictive <- pp_check(fit_year, ndraws = 100, resp = 'nreassortants') + 
+  theme_minimal()
+
+
+
+fit_priorpredictive$data %<>%
+  mutate(value = log1p(value))
+
+fit_priorpredictive
 
 # Fit Model
 diffusionmodel1_fit <- brm(
