3432a475c4150b0e678a260e2c068aa8dd58925d
James Baxter <james.baxter@ed.ac.uk>
2024-09-24 20:13:40 +0100

    need to find a way to fix cluster asignation

--- scripts/clustering_model.R
+++ scripts/clustering_model.R
@@ -15,11 +15,10 @@
 # is conducted using OLS regression, thereafter progressing to Bayesian analysis using BRMS.
 
 ########################################## DEPENDENCIES ############################################
-library(brms)
 library(broom)
 library(broom.mixed)
-library(tidybayes)
-library(bayesplot)
+library(tidyverse)
+library(recipes)
 
 
 ########################################### IMPORT DATA ############################################
@@ -28,17 +27,29 @@ combined_data <- read_csv('./2024Aug18/treedata_extractions/2024-09-20_combined_
 summary_data <- read_csv('./2024Aug18/treedata_extractions/summary_reassortant_metadata_20240904.csv') %>%
   select(-c(cluster_label,
             clade)) 
-  
+
+# import colour schemes
+host_colour <- read_csv('./colour_schemes/hostType_cols.csv')
+region_colour <- read_csv('./colour_schemes/regionType_cols.csv')
+riskgroup_colour <- read_csv('./colour_schemes/riskgroup_cols.csv') %>%
+  mutate(kclust = c(2,1, 3),
+         group2 = c('major', 'minor', 'dominant'))
+subtype_colour <- read_csv('./colour_schemes/SubType_cols.csv')
 
 ########################################### FORMAT DATA ############################################
 kclust_nophylo_data <- summary_data %>%
   
-  select(-c(contains('range'),
-            Kcluster, 
-            if_mammal,
-            check,
-            group,
-            starts_with('col'))) %>% 
+  # criteria selected by Lu
+  select(c(
+    cluster_profile,
+    group2,
+    host_richness,
+    if_mammal,
+    max_distance_km,
+    Num_Sequence,
+    number_Conti,
+    Length_Between_First_Last_Sample
+  )) %>% 
   
   # All numeric NA as 0
   mutate(across(where(is.double), .fns = ~ replace_na(.x, 0))) %>%
@@ -50,8 +61,8 @@ kclust_nophylo_data <- summary_data %>%
   step_normalize(all_numeric()) %>% 
   
   # create dummy variables for categorical predictor
-  step_dummy(Continent_of_Earliest_Date, one_hot = TRUE) %>%
-  step_dummy(Host_of_Earliest_Sample_Date, one_hot = TRUE) %>%
+  #step_dummy(Continent_of_Earliest_Date, one_hot = TRUE) %>%
+  #step_dummy(Host_of_Earliest_Sample_Date, one_hot = TRUE) %>%
   
   # run tidymodels recipe
   prep() %>%
@@ -121,6 +132,7 @@ kclust_updated_data <- combined_data %>%
 
 
 ####################################### START KCLUST PIPELINE ########################################
+set.seed(4472)
 kclust_nophylo <- tibble(k = 1:20) %>%
   mutate(
     kclust = map(k, ~kmeans(kclust_nophylo_data %>% select(-c(group2, cluster_profile)), .x)),
@@ -142,23 +154,22 @@ clusterings_nophylo <-
   unnest(cols = c(glanced))
 
 clustersnophylo_plot <- assignments_nophylo %>%
-  filter(k ==3) %>%
+  filter(k == 4) %>%
   select(-c(kclust, tidied, glanced)) %>%
   recipe(~ .) %>%
   step_pca(all_numeric(), -k, num_comp = 2) %>%  # Perform PCA (e.g., 2 components)
   prep() %>%
   bake(NULL)%>%
   ggplot(., aes(x = PC1, y = PC2)) +
-  geom_point(aes(colour = .cluster, shape = group2), alpha = 0.8) + 
-  scale_shape('Pre-Assigned Classification') + 
-  scale_color_discrete('K-Means Clusters') +
-  theme_minimal() +
+  geom_point(aes(colour = .cluster), size = 4, alpha = 0.8) + 
+  theme_minimal(base_size = 20) + 
+  scale_color_manual(
+    'K-Means Clusters',
+    values = riskgroup_colour %>% pull(Trait, name = kclust), 
+    labels = riskgroup_colour %>% pull(group2, name = kclust)) +
   theme(legend.position = 'bottom',
-        legend.box = "vertical") +               # Stack legends vertically
-  guides(
-    color = guide_legend(order = 1),             # Ensure color legend is first
-    shape = guide_legend(order = 2)              # Shape legend is second
-  )
+        legend.box = "vertical")            #
+
 
 ggplot(clusterings_nophylo, aes(k, tot.withinss)) +
   geom_line() +
@@ -168,6 +179,92 @@ ggplot(clusterings_nophylo, aes(k, tot.withinss)) +
   scale_x_continuous('K', breaks = seq(1,20, by = 1))
 
 
+
+
+###################################################################################################
+# data + labels
+
+# data points used for clustering and cluster_profiles
+labelled_nophylo <- assignments_nophylo %>%
+  filter(k == 4) %>%
+  select(-c(group2,
+            kclust,
+            tidied,
+            glanced,
+            k,
+            .cluster)) 
+
+
+
+# generate permutations for all columns (except 1 - the cluster profile label)
+kclust_permutations <- list()
+
+for (i in 2:ncol(labelled_nophylo)){
+  kclust_permutations[[i]] <- labelled_nophylo %>%
+    permutations(permute = all_of(i), times = 10)
+}
+
+names(kclust_permutations) <- colnames(labelled_nophylo)
+
+# bind together permutaed data in a single dataframe, and nest
+kclust_permutations %<>% 
+  bind_rows(., .id = 'permuted_var') %>%
+  unite(id, permuted_var, id)  %>%
+  mutate(data = map(splits, ~ analysis(.x))) %>%
+  select(-splits) %>%
+  unnest(data) 
+
+
+
+# execute kmeans on nested data (ie one run of kmeans per permuted dataset)
+permuted_nophylo <- kclust_permutations %>%
+  #select(-cluster_profile) %>%
+  nest(., .by = id) %>%
+  mutate(
+    kclust = map(data,  ~select(.x , -cluster_profile) %>% kmeans(3)),
+    tidied = map(kclust, tidy),
+    glanced = map(kclust, glance),
+    augmented = map2(kclust, data, augment)
+  ) 
+  
+
+# extract results
+permuted_clusters_nophylo <- permuted_nophylo  %>%
+  unnest(cols = c(tidied)) 
+
+permuted_assignments_nophylo <- permuted_nophylo  %>% 
+  unnest(cols = c(augmented)) 
+ 
+permuted_clusterings_nophylo <- permuted_nophylo  %>%
+  unnest(cols = c(glanced))
+
+
+# was cluster assignment correct according to baseline kmeans?
+lookup_clusters <- assignments_nophylo %>%
+  filter(k == 3) %>%
+  select(c(cluster_profile,
+           .cluster)) %>%
+  rename(original_cluster = .cluster)
+
+
+test <- permuted_assignments_nophylo %>%
+  select(c(cluster_profile,
+           .cluster,
+           id)) %>%
+  left_join(lookup, by = join_by(cluster_profile)) %>%
+  mutate(across(ends_with('cluster'), .fns = ~as.integer(.x))) %>%
+  mutate(is_correct = case_when(.cluster == original_cluster ~ 1,
+                                .cluster != original_cluster ~ 0)) %>%
+  separate_wider_delim(id, delim = regex('_(?!.*_)'), 
+                       names = c('var', 'permutation')) %>%
+  summarise(prop_correct = mean(is_correct),
+            .by = c(var, permutation))
+
+
+
+
+
+# 
 ########
 
 kclusts <- tibble(k = 1:20) %>%
@@ -204,8 +301,8 @@ assignments %>%
   }) %>%
   bind_rows() %>%
   ggplot(., aes(x = PC1, y = PC2)) +
-  geom_point(aes(colour = .cluster, shape = group2), alpha = 0.8) + 
-  theme_minimal() +
+  geom_point(aes(colour = .cluster, shape = group2), size = 4, alpha = 0.8) + 
+  theme_minimal(base_size = 20) +
   facet_wrap(~ k)
 
 clustersphylo_plot <- assignments %>%
