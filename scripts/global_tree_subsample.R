189df63f334917ea86e2ff1f543c3c22fa130864
James Baxter <james.baxter@ed.ac.uk>
2025-01-06 18:54:54 +0000

    aim to merge global_tree_subsample and get_global_alignments

--- scripts/global_tree_subsample.R
+++ scripts/global_tree_subsample.R
@@ -23,11 +23,31 @@ library(magrittr)
 # User functions
 
 ############################################## DATA ################################################
+all_meta <- read_csv('./2024-09-09_meta.csv')
 
+alignments <- c(list.files('./2024Aug18/reassortant_alignments',
+                         pattern = 'ha'),
+                grep(list.files('./2024Aug18/region_alignments',
+                                pattern = 'ha_'), pattern='southamerica', invert=TRUE, value=TRUE),
+                list.files('./2024Sept16/south_america',
+                           pattern = 'ha_.*fasta'))
 
 ############################################## MAIN ################################################
 
+ha_global_subsample <- all_meta %>% 
+  drop_na(collection_date) %>% 
+  filter(grepl('H5', virus_subtype)) %>% 
+  group_by(collection_datemonth, collection_regionname) %>% 
+  slice_sample(n = 2)
 
+pb2_global_subsample <- all_meta %>% 
+  drop_na(collection_date) %>% 
+  #filter(grepl('H5', virus_subtype)) %>% 
+  group_by(collection_datemonth, collection_regionname) %>% 
+  slice_sample(n = 1)
+
+# HA subsample: n = 948
+# PB2 subsample: n = 839
 ############################################## WRITE ################################################
 
 
