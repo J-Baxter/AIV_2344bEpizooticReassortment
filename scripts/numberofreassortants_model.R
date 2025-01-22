220a0b3c1343f59c6b835387714895d00ba695e1
James Baxter <james.baxter@ed.ac.uk>
2025-01-22 19:06:42 +0000

    count data now includes season and changes 'previous' reassortant to previous most significant reassortant. Refactoring of model code, updated prior distributions to reflect additional terms and change of backend to 'cmdstanr' (additional dependency required)

--- scripts/numberofreassortants_model.R
+++ scripts/numberofreassortants_model.R
@@ -33,6 +33,28 @@ library(ggmcmc)
 
 # User functions
 
+FormatContinent <- function(dataframe){
+  dataframe %<>%
+    mutate(collection_regionname = case_when(grepl('europe', 
+                                                   collection_regionname, 
+                                                   ignore.case = TRUE) ~ 'europe',
+                                             
+                                             grepl('africa',
+                                                   collection_regionname, 
+                                                   ignore.case = TRUE) ~ 'africa',
+                                             
+                                             grepl('asia', 
+                                                   collection_regionname, 
+                                                   ignore.case = TRUE) ~ 'asia',
+                                             
+                                             grepl('(central|northern) america', 
+                                                   collection_regionname, 
+                                                   ignore.case = TRUE) ~ 'central & northern america',
+                                             
+                                             .default = NA_character_ ))
+  
+  return(dataframe)
+}
 
 ############################################## DATA ################################################
 combined_data <- read_csv('./2024Aug18/treedata_extractions/2024-09-20_combined_data.csv')
@@ -40,99 +62,44 @@ combined_data <- read_csv('./2024Aug18/treedata_extractions/2024-09-20_combined_
 
 ############################################## MAIN ################################################
 # Data pre-processing
+
+# Total number of reported H5 Highly Pathogenic Avian Influenza
+# (Source: https://empres-i.apps.fao.org/diseases)
+
 h5_hpai_counts <- read_csv('./2025Jan17_faoHPAI.csv') %>%
   filter(grepl('Avian', Disease) & grepl('H5N', Serotype)) %>%
-  mutate(Region = case_when(grepl('Europe', 
-                                  Region ) ~ 'europe',
-                            grepl('Africa',
-                                  Region ) ~ 'africa',
-                            grepl('Asia', 
-                                  Region ) ~ 'asia',
-                            grepl('America', 
-                                  Region ) ~ 'central & northern america',
-                            .default = Region )) %>%
-  mutate(Observation.date = dmy(Observation.date) %>% format.Date('%Y-%m')) %>%
-  filter(Observation.date > '2016-12-31') %>%
-  drop_na(Observation.date) %>%
-  summarise(n_cases = n(), .by = c(Region, Observation.date)) %>%
-  arrange(Observation.date) %>%
   rename(collection_monthyear = Observation.date,
-         collection_regionname = Region)
-  
+         collection_regionname = Region) %>%
+  FormatContinent() %>%
+  mutate(collection_monthyear = dmy(collection_monthyear) %>% format.Date('%Y-%m')) %>%
+  filter(collection_monthyear > '2016-05-31' & collection_monthyear < '2024-05-01') %>%
+  drop_na(collection_monthyear, collection_regionname) %>%
+  summarise(n_cases = n(), .by = c(collection_regionname, collection_monthyear)) %>%
+  arrange(collection_monthyear)
 
 
+# Number of sequences 
 numberofsequence_data <- read_csv('./2024-09-09_meta.csv') %>%
   filter(grepl('[hH]5', virus_subtype)) %>%
   rename(collection_monthyear = collection_datemonth) %>%
+  filter(collection_date > '2016-05-31') %>%
   select(c(collection_monthyear,
            collection_regionname,
            cluster_profile)) %>%
   drop_na(collection_monthyear) %>%
-  mutate(collection_regionname = case_when(grepl('europe', 
-                                                 collection_regionname) ~ 'europe',
-                                           grepl('africa',
-                                                 collection_regionname) ~ 'africa',
-                                           grepl('asia', 
-                                                 collection_regionname) ~ 'asia',
-                                           grepl('(central|northern) america', 
-                                                 collection_regionname) ~ 'central & northern america',
-                                           .default = collection_regionname)) %>%
-  #mutate(collection_year = 
-           #paste0(collection_datemonth, '-01') %>%
-          # ymd() %>%
-           #decimal_date() %>%
-           #round(digits = 1)) %>%
-  summarise(n_sequences = n(), .by = c(collection_monthyear, collection_regionname))
+  FormatContinent() %>%
+  arrange(collection_regionname, ym(collection_monthyear)) %>%
+  summarise(n_sequences = n(), 
+            .by = c(collection_monthyear, collection_regionname))
 
-
-counts <- h5_hpai_counts %>%
-  full_join(numberofsequence_data) %>%
-  replace_na(list(n_case = 0, n_sequences = 0))
-
-# Stratified by region (for supplementary)
-counts %>%
-  filter(collection_regionname %in% c('europe', 'asia','central & northern america','africa')) %>%
-  pivot_longer(starts_with('n_'), names_to = 'var', values_to = 'value') %>%
-  mutate(collection_monthyear = ym(collection_monthyear)) %>%
-  filter(collection_monthyear > '2017-01-01') %>%
-  ggplot(aes(x=collection_monthyear, y= value, colour = var)) + 
-  geom_bar(aes(x = ym(collection_monthyear), y = n_reassortants*30), stat = 'identity', data = count_data %>% filter(collection_monthyear > '2017-01-01'), inherit.aes = FALSE) + 
-  geom_point(size =1) +
-  geom_line(aes(group = var)) +
-  facet_grid(rows = vars(collection_regionname)) + 
-  scale_x_date()  +
-  scale_y_continuous(sec.axis = sec_axis( trans=~./30, name="Reassortment")) +
-  global_theme
-
-# Global (for supplementary)
-counts %>%
-  filter(collection_regionname %in% c('europe', 'asia','central & northern america','africa')) %>%
-  pivot_longer(starts_with('n_'), names_to = 'var', values_to = 'value') %>%
-  summarise(value = sum(value), .by =  c(collection_monthyear, var)) %>%
-  mutate(collection_monthyear = ym(collection_monthyear)) %>%
-  filter(collection_monthyear > '2017-01-01') %>%
-  ggplot(aes(x=collection_monthyear, y= value, colour = var)) + 
-  geom_bar(aes(x = ym(collection_monthyear), y = n_reassortants*30), stat = 'identity', data = count_data %>% filter(collection_monthyear > '2017-01-01'), inherit.aes = FALSE) + 
-  geom_point(size =1) +
-  geom_line(aes(group = var)) +
-  scale_x_date()  +
-  scale_y_continuous(sec.axis = sec_axis( trans=~./30, name="Reassortment"))+
-  global_theme
-
-
-count_data <- combined_data %>%
+# Number and TMRCA of reassortants as inferred from our phylodynamic analysis
+reassortant_counts <- combined_data %>%
+  
+  # restrict to HA for now
   filter(segment == 'ha') %>%
   mutate(collection_monthyear = date_decimal(TMRCA) %>% 
            format(., "%Y-%m")) %>%
-  mutate(collection_regionname = case_when(grepl('europe', 
-                                                 collection_regionname) ~ 'europe',
-                                           grepl('africa',
-                                                 collection_regionname) ~ 'africa',
-                                           grepl('asia', 
-                                                 collection_regionname) ~ 'asia',
-                                           grepl('(central|northern) america', 
-                                                 collection_regionname) ~ 'central & northern america',
-                                           .default = collection_regionname)) %>%
+
   filter(!is.na(collection_regionname)) %>%
   select(collection_regionname,
          collection_monthyear, 
@@ -141,42 +108,127 @@ count_data <- combined_data %>%
   summarise(n_reassortants = n_distinct(cluster_profile), 
             .by = c(collection_monthyear,
                     collection_regionname,
-                    group2))  %>%
-  #mutate(collection_year = collection_monthyear %>%
-         #  paste0('-01') %>%
-          # ymd() %>%
-           #decimal_date() %>%
-           #round(., digits = 1))  %>%
-  summarise(n_reassortants = sum(n_reassortants), 
-            .by = c(collection_monthyear,collection_regionname, group2)) %>%
+                    group2
+            ))  %>%
   
-  #arrange(collection_year) %>%
-  left_join(counts)
+  # within each continent...
+  arrange(collection_regionname, collection_monthyear) %>%
+  group_by(collection_regionname) %>%
+  
+  
+  # Get the class of t-1 reassortant
+  pivot_wider(names_from = group2, values_from = n_reassortants) %>%
+
+  mutate(joint_reassortant_class = case_when(
+    minor > 0 & is.na(major) & is.na(dominant) ~ 'minor',
+    is.na(minor) & major > 0 & is.na(dominant) ~ 'major',
+    is.na(minor)  & is.na(major) & dominant > 0 ~ 'dominant',
+    minor > 0 & major > 0 & is.na(dominant) ~ 'minor_major',
+    is.na(minor) & major > 0 & dominant > 0 ~ 'major_dominant',
+    minor > 0 & is.na(major) & dominant > 0 ~ 'minor_dominant',
+    minor > 0 & major > 0 & dominant > 0 ~ 'minor_major_dominant',
+    .default = NA)) %>%
+  
+  fill(joint_reassortant_class, .direction = 'down') %>%
+  mutate(previous_reassortant_class = lag(joint_reassortant_class)) %>%
+  
+  pivot_longer(cols = c(minor, major,  dominant), 
+               names_to = 'reassortant_class',
+               values_to = 'n_reassortants',
+               values_drop_na = TRUE)
+
+
+count_data <- expand_grid(collection_monthyear = seq.Date(from = ym('2016-06'),
+                                                          to =  ym('2024-06'),
+                                                          by = "month"),
+                          collection_regionname = unique(reassortant_counts$collection_regionname)) %>%
+  mutate(collection_monthyear = format.Date(collection_monthyear, "%Y-%m")) %>%
+  
+  left_join(reassortant_counts) %>%
+  left_join(numberofsequence_data) %>%
+  left_join(h5_hpai_counts) %>%
+  
+  
+  separate_wider_delim(collection_monthyear, '-', names = c('collection_year', 'collection_month'),   cols_remove = FALSE) %>%
+  mutate(across(c('collection_month', 'collection_year'), .fns = ~ as.double(.x))) %>%
+  mutate(time = ym(collection_monthyear) %>% decimal_date()) %>%
+  
+  # within each continent...
+  group_by(collection_regionname) %>%
+  
+  
+  # And infer the time since the last dominant reassortant
+  mutate(last_dominant = if_else(grepl('dominant', reassortant_class), time, NA)) %>%
+  fill(last_dominant) %>%
+  mutate(time_since_last_dominant = as.numeric(time - last_dominant)) %>%
+  
+  # Classify tmrca month according to breeding season
+  mutate(collection_season = case_when(collection_month %in% c(12,1,2) ~ 'overwintering', 
+                                       collection_month %in% c(3,4,5)  ~ 'migrating_spring', # Rename to spring migration
+                                       collection_month %in% c(6,7,8)  ~ 'breeding', 
+                                       collection_month %in% c(9,10,11)  ~ 'migrating_autumn' # Rename to autumn migration
+  )) %>%
+  
+  # select variables
+  ungroup(collection_regionname) %>%
+  select(collection_regionname,
+         collection_year,
+         collection_month,
+         collection_season, 
+         collection_monthyear,
+         n_cases,
+         n_sequences,
+         n_reassortants,
+         reassortant_class,
+         joint_reassortant_class,
+         previous_reassortant_class,
+         time,
+         time_since_last_dominant,
+  ) %>%
+  
+  
+  # scaling
+  mutate(across(c(time, collection_year), .fns = ~ subtract(.x, 2015))) %>%
+  
+  # set default NA values
+  replace_na(list(n_cases = 0, 
+                  n_sequences = 0,
+                  n_reassortants = 0,
+                  time_since_last_dominant = 0, # this may be quite a strong assumption (accidentally)
+                  reassortant_class = 'none',
+                  last_reassortant_class = 'none',
+                  joint_reassortant_class = 'none',
+                  previous_reassortant_class = 'none'
+  )) 
+
 
 
 # Model Formula
-bf1 <- bf(n_reassortants|resp_trunc(lb = 1) ~ 1 + collection_regionname + (1|collection_year),
-          family = poisson())
+bf1 <- bf(n_reassortants ~ collection_regionname + n_cases  +  collection_season + previous_reassortant_class + (1 + collection_regionname + collection_month|collection_year),
+          zi ~ collection_regionname + n_cases  + (1 + collection_regionname + collection_month|collection_year),
+          family = zero_inflated_negbinomial())
 
-bf2 <- bf(group2 ~ 1 + collection_regionname + (1|collection_year), 
-          family = categorical())
 
+
+bf2 <- bf(reassortant_class ~  collection_regionname + previous_reassortant_class + (1 + collection_regionname + collection_month|collection_year), 
+          family = categorical(link  = 'logit'))
 count_formula <- mvbf(bf1, bf2)
 
 
 # Define Priors
-countmodel_priors <- get_prior(count_formula,
-                               data = count_data,
-                               family = list(zero_inflated_negbinomial(), categorical())) 
+mv <- mvbf(bf1, bf2)
 
+countmodel_mv_priors <- get_prior(mv, count_data)
 
-countmodel_priors$prior[c(1:6,11:14,19:22)] <- "normal(0,5)"
+#countmodel_mv_priors[c(1, 4:15,25:29, 38:44, 53:59, 69:74),1] <- "normal(0,5)"
+countmodel_mv_priors
+countmodel_mv_priors[c(1, 4:15, 25:29, 38:44, 53:59, 69:74),1] <- "normal(0,5)"
 
 
 # Set MCMC Options
 CHAINS <- 4
 CORES <- 4
-ITER <- 4000
+ITER <- 16000
 BURNIN <- ITER/10 # Discard 10% burn in from each chain
 SEED <- 4472
 THREADS <- 2
@@ -186,48 +238,50 @@ THREADS <- 2
 # Note that due to weakly informative priors, the prior predictive checks reveal little about the 
 # ability of our priors to describe the data. 
 countmodel_priorpredictive <- brm(
-  count_formula,  # Multivariate outcome
-  data = count_data,           # Dataframe containing y1, y2, x1, x2
-  prior = countmodel_priors,
+  mv,
+  data = count_data,           
+  prior = countmodel_mv_priors,
   chains = CHAINS,
+  threads = THREADS, 
   cores = CORES, 
-  threads = THREADS,
-  iter = ITER,
-  warmup = BURNIN,
+  iter = 16000,
+  warmup = 1600,
   seed = SEED,
+  backend = "cmdstanr",
   sample_prior = "only",
   control = list(adapt_delta = 0.95)
 )
 
 
 # Fit model to data
-countmodel_fit <- brm(
-  count_formula,  # Multivariate outcome
-  data = count_data,           # Dataframe containing y1, y2, x1, x2
-  prior = countmodel_priors,
+countmodel_mv_fit <- brm(
+  mv,
+  data = count_data,           
+  prior = countmodel_mv_priors,
   chains = CHAINS,
   threads = THREADS, 
   cores = CORES, 
-  iter = ITER,
-  warmup = BURNIN,
+  iter = 16000,
+  warmup = 1600,
   seed = SEED,
-  control = list(adapt_delta = 0.95)
-)
+  backend = "cmdstanr",
+  control = list(adapt_delta = 0.97))
+
 
 # Post-fitting diagnostics (including inspection of ESS, Rhat and posterior predictive)
-tidy_countmodel_fit <- tidy(countmodel_fit)
+tidy_countmodel_fit <- tidy(countmodel_mv_fit)
 #posteriorpredictive <-pp_check(countmodel_fit, ndraws = 500)
 
 
 # Misc evaluations
 #print(prior_summary(countmodel_fit), show_df = FALSE)
-performance(countmodel_fit) # tibble output of model metrics including R2, ELPD, LOOIC, RMSE
-plot(countmodel_fit) # default output plot of brms showing posterior distributions of
-prior_summary(countmodel_fit) #obtain dataframe of priors used in model.
+performance(countmodel_mv_fit) # tibble output of model metrics including R2, ELPD, LOOIC, RMSE
+plot(countmodel_mv_fit) # default output plot of brms showing posterior distributions of
+prior_summary(countmodel_mv_fit) #obtain dataframe of priors used in model.
 
 mcmc_countmodel_fit <- ggs(countmodel_fit) # Warning message In custom.sort(D$Parameter) : NAs introduced by coercion
 
-posteriorpredictive_group <-pp_check(countmodel_fit, ndraws = 500, resp = 'group2')
+posteriorpredictive_group <-pp_check(countmodel_fit, ndraws = 500, resp = 'reassortantclass')
 posteriorpredictive_nreassortants <-pp_check(countmodel_fit, ndraws = 500, resp = 'nreassortants')
 
 
@@ -310,11 +364,25 @@ ggplot(class_preds) +
   scale_x_continuous(limits = c(0,1))
 
 
-# Required outputs: Pre/post epizootic, marginal effect of region (ie how many more reassortants in a vs b),
+# Required outputs: Pre/post epizootic, marginal effect of region ,
 # 
 
 
-# Marginal probability of N reassortants / year ~ 1|Region
+# Posterior prediction of the number and type of reassortant at 'average' parameters (global and stratified by region)
+
+
+# marginal effect of of continent on number and type of reassortant (ie how many more reassortants in a vs b)
+
+
+# reassortant 'transition' probabilities (ie based on lagged reassortant type), global and stratified by region
+
+
+# reassortant  probabilities according to time since last dominant (stratified by region)
+# Random Effects:
+
+# Year (does the probability of number and type change over each year per continent)
+
+
 
 
 # Marginal effect of region
