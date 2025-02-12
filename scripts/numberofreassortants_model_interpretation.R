e615c403b4d0e9471c15c5c13bad992d9c382d58
James Baxter <39841697+J-Baxter@users.noreply.github.com>
2025-02-12 19:19:03 +0000

    comprehensive changes to all plots

--- scripts/numberofreassortants_model_interpretation.R
+++ scripts/numberofreassortants_model_interpretation.R
@@ -60,6 +60,66 @@ countmodel_posteriorpredictive <- bind_rows(posteriorpredictive_nreassortants$da
 
 
 
+
+plt_c <- 
+  
+countmodel_mv_fit %>% 
+  epred_draws(newdata = count_data %>%
+                select(collection_season) %>%
+                distinct(),
+              resp = "nreassortants",
+              re_formula = NA)
+
+countmodel_mv_fit %>% 
+  emmeans(~collection_season,
+          re_formula = NA,
+          dpar = 'nreassortants',
+          epred = TRUE)
+
+countmodel_mv_fit %>% 
+  add_predicted_draws(newdata = count_data %>%
+                        select(collection_regionname) %>%
+                        distinct(),
+                      re_formula = NA)
+
+
+countmodel_mv_fit %>% 
+  emmeans(~ collection_regionname,
+          at = list(collection_regionname = count_data %>%
+                      select(collection_regionname)),
+                      re_formula = NA)
+
+
+countmodel_mv_fit %>% 
+  add_predicted_draws()
+
+joint_preds <- expand_grid(collection_regionname = count_data %>%
+              pull(collection_regionname) %>% 
+              unique(),
+            
+            collection_season = count_data %>%
+              pull(collection_season) %>% 
+              unique(),
+            
+            n_cases = 20,
+            
+            previous_reassortant_class = count_data %>%
+              pull(previous_reassortant_class) %>% 
+              unique()
+        
+            ) %>%
+  add_predicted_draws(countmodel_mv_fit,re_formula = NA)%>%
+  pivot_wider(names_from = .category, values_from = .prediction) %>%
+  ungroup()%>%
+  summarise(n = n(),.by = c(collection_regionname, nreassortants, reassortantclass))%>%
+  mutate(freq=n/sum(n))
+
+
+
+  
+
+  
+
 # Conditional prediction draws  + Conditional marginal means stratified by continent
 
 # Required outputs: Pre/post epizootic, marginal effect of region ,
@@ -73,35 +133,227 @@ countmodel_posteriorpredictive <- bind_rows(posteriorpredictive_nreassortants$da
 
 
 # reassortant 'transition' probabilities (ie based on lagged reassortant type), global and stratified by region
+predict_season <- countmodel_mv_fit %>%
+  emmeans( ~ collection_season,
+          var = "nreassortants",
+          at = list(collection_season = unique(count_data$collection_season)),
+          epred = TRUE, 
+          dpar = 'mu_nreassortants',
+          re_formula = NA, 
+          regrid = "response",
+          #allow_new_levels = TRUE
+          ) %>% 
+  gather_emmeans_draws()
+
+ggplot(predict_season, aes(x = .value, y= collection_season)) +
+  stat_slabinterval()
+  
 
 
-# reassortant  probabilities according to time since last dominant (stratified by region)
-# Random Effects:
-
-# Year (does the probability of number and type change over each year per continent)
-
-
-
-
-# Marginal effect of region
-
-
-# Marginal class probability  
-
-
-# Marginal class probabilty ~ 1| Region
-
-
-# Joint Probability
+countmodel_mv_fit %>%
+  emmeans( ~ collection_regionname,
+           #var = "nreassortants",
+           at = list(collection_regionname= unique(count_data$collection_regionname)),
+           epred = TRUE, 
+           #dpar = 'mu_nreassortants',
+           re_formula = NA, 
+           regrid = "response",
+           #allow_new_levels = TRUE
+  ) %>% 
+  gather_emmeans_draws()
 
 
 
-# Joint Probability ~ 1|Region
+test <- countmodel_mv_fit %>%
+  emmeans( ~ previous_reassortant_class,
+           at = list(),
+           #epred = TRUE, 
+           dpar = 'reassortantclass',
+          re_formula = NA,
+          dpar ='reassortantclass',
+          regrid = "response",
+           #allow_new_levels = TRUE
+  )  %>% 
+  gather_emmeans_draws()
+
+avg_predictions(countmodel_mv_fit)
 
 
-# Marginal effect of region
 
 
+
+ggplot(aes(x  = 1-.value,
+           y = season,
+           slab_colour = season,
+           slab_fill = season)) +
+  stat_halfeye(slab_alpha = 0.7,
+               p_limits = c(0.001, 0.999),
+               point_interval = "median_hdi",
+               linewidth = 1.5,
+               .width =  0.95)
+
+
+
+countmodel_mv_fit %>%
+  conditional_effects(., effects = '')
+##############################################
+
+test <- countmodel_mv_fit %>%
+  add_epred_draws(newdata = expand_grid(previous_reassortant_class = unique(count_data$previous_reassortant_class),
+                                        collection_regionname = unique(count_data$collection_regionname)),
+                  re_formula = NA,
+                  resp = 'reassortantclass')
+
+tidy_dag <- test %>%
+  filter(previous_reassortant_class != 'none') %>%
+  filter(.category != 'none') %>%
+  median_hdci() %>%
+  rename(name = previous_reassortant_class, 
+         to = .category) %>% 
+  as_tidy_dagitty()  #%>%
+  #mutate(circular = case_when(name == to ~ yend - TRUE, .default = FALSE)) 
+  #rename(from = name) %>% 
+  #igraph::graph_from_data_frame(., directed = TRUE) %>%
+  #plot()
+  
+test %>%
+  filter(previous_reassortant_class != 'none') %>%
+  filter(.category != 'none') %>%
+  ungroup(previous_reassortant_class) %>%
+  median_hdi() 
+
+##########################################
+
+
+
+
+emms <- emmeans(
+
+  countmodel_mv_fit, ~ reassortantclass | previous_reassortant_class,  resp = "reassortantclass"                    
+)
+
+# Summarize probabilities
+summary(emms, type = "response")
+plt_5e <- predict_hu_season %>%
+  gather_emmeans_draws()
+
+countmodel_mv_fit %>%
+  predicted_draws(newdata = tibble(previous_reassortant_class  = unique(count_data$previous_reassortant_class)),
+                  re_formula = NA,
+                  dpar ='munone_reassortantclass')
+
+  add_epred_draws(.,
+                  )
+
+
+  
+##### PLOTS #####
+plt_a <- count_data %>%
+  mutate(collection_monthyear = ym(collection_monthyear)) %>%
+  filter(reassortant_class != 'none') %>%
+  ggplot() + 
+  geom_histogram(aes(x = collection_monthyear, 
+                     fill = collection_regionname, 
+                     colour = collection_regionname,
+                     alpha = reassortant_class))  +
+  scale_x_date('Reassortant Emergence',expand = c(0,0))  +
+  scale_y_continuous('Count',expand = c(0,0)) +
+    scale_fill_manual('Continent' ,values = region_colours,
+                      labels = str_to_title) +
+    scale_colour_manual('Continent' , values = region_colours,
+                        labels = str_to_title) +
+    scale_alpha_manual('Reassortant Class' , values = c('dominant' = 1, 'major' = 0.7, 'minor' = 0.5),
+                       labels = str_to_title) +
+  global_theme + 
+  theme(legend.position = 'inside',
+        legend.position.inside = c(0.2,0.7))
+
+
+plt_b <- count_data %>%
+  filter(reassortant_class != 'none') %>%
+  summarise(n = n(), .by = c(collection_regionname, reassortant_class)) %>%
+  group_by(collection_regionname) %>%
+  mutate(freq = n/sum(n)) %>%
+  ggplot() + 
+  geom_bar(stat = 'identity',
+           aes(x = collection_regionname,
+               y = freq,
+               fill = collection_regionname, 
+               colour = collection_regionname,
+               alpha = reassortant_class))  + 
+  #scale_fill_brewer('Reassortant Class', palette = 'Set1') + 
+  #scale_colour_brewer('Reassortant Class', palette = 'Set1') + 
+  scale_fill_manual(values = region_colours) +
+  scale_colour_manual(values = region_colours) +
+  
+  scale_alpha_manual(values = c('dominant' = 1, 'major' = 0.7, 'minor' = 0.5)) +
+  scale_x_discrete('Continent',expand = c(0,0), labels = str_to_title)  +
+  scale_y_continuous('Proportion',expand = c(0,0)) +
+  global_theme
+
+
+# joint predictions
+plt_c <- joint_preds %>% 
+  #mutate(freq=n/sum(n)) %>%
+  mutate(reassortantclass = case_when(reassortantclass == 3 ~ 'minor', 
+                                      reassortantclass == 1 ~ 'dominant',
+                                      reassortantclass == 2 ~ 'major')) %>%
+  filter(nreassortants != 0) %>% 
+  filter(freq > 0.001) %>% 
+  drop_na(reassortantclass) %>%
+  ggplot() + 
+  geom_point(aes(y = nreassortants, 
+                 x = as.character(reassortantclass), 
+                 size = freq,
+                 fill = collection_regionname,
+                 colour = collection_regionname,
+                 alpha = reassortantclass)) +
+  facet_grid(cols= vars(collection_regionname), labeller = labeller(collection_regionname = str_to_title)) + 
+  scale_y_continuous('N' , breaks= c(1,3,5,7), limits = c(1,7)) +
+  scale_x_discrete('Reassortant Class', labels = str_to_title) +
+  scale_fill_manual(values = region_colours) +
+  scale_colour_manual(values = region_colours) +
+  
+  scale_alpha_manual(values = c('dominant' = 1, 'major' = 0.7, 'minor' = 0.5)) +
+  global_theme
+
+
+# transition probabilities
+plt_d <- tidy_dag %>%
+  mutate(name = case_when(name == 'dominant'~ 'D',
+                          name == 'major' ~ "Mj",
+                          name == 'minor' ~ 'Mn')) %>%
+  ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_dag_point(aes(alpha = name, colour = collection_regionname)) +
+  geom_dag_text(size = 3) +
+  geom_dag_edges_fan(aes(edge_width = .epred*2,
+                         label = paste0(round(.epred, digits = 3), 
+                                        ' (', round(.lower, digits = 3),
+                                        '-', round(.upper, digits = 3), ')')), 
+                     spread = 5,
+                     label_size = 3,
+                     label_dodge	= 0.5) + 
+  #geom_dag_edges_arc(aes(edge_width = .epred))+ 
+  facet_grid(cols = vars(collection_regionname),
+             labeller = labeller(collection_regionname= str_to_title)) + 
+  scale_alpha_manual(values = c('D' = 1, 'Mj' = 0.7, 'Mn' = 0.5)) +
+  scale_colour_manual(values = region_colours) +
+  theme_dag(legend.position = 'none', base_size = 8)
+
+
+
+plots <- align_plots(plt_a, plt_c, plt_d, align = 'v', axis = 'l')
+
+
+plot_grid(plot_grid(plots[[1]], plt_b, ncol = 2, labels = 'AUTO'), plots[[2]], plots[[3]], labels = c('', 'C', 'D'), nrow = 3)
+
+
+ggsave('~/Downloads/figure6.jpeg',
+       height = 30,
+       width = 35,
+       dpi= 360,
+       units= 'cm')
+# number of reassortants ~ season
 
 ##### Plot Posterior Predictive Check #####
 pp_a <- countmodel_posteriorpredictive  %>%
