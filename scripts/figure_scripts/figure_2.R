d1af6922badec5ea8f2046401a41272814bf4d15
James Baxter <james.baxter@ed.ac.uk>
2025-03-18 17:16:27 +0000

    corrected cumulative count plot (counts now extend to present day); implemented line colour labels (already deprecated); added legend to A

--- scripts/figure_scripts/figure_2.R
+++ scripts/figure_scripts/figure_2.R
@@ -17,13 +17,12 @@ memory.limit(30000000)
 # Packages
 library(tidyverse)
 library(magrittr)
-
+library(geomtextpath)
 
 # User functions
 
 
 ############################################## DATA ################################################
-
 h5_diversity_sliding_window <- read_csv('./h5_diversity_sliding_window.csv')
 combined_data <- read_csv('./2024Aug18/treedata_extractions/2024-09-20_combined_data.csv')
 
@@ -36,12 +35,18 @@ region_colours <- c('europe' = '#1b9e77',
                     'south america' ='#e6ab02')
 
 # Cumulative number of reassortants
-plt_2a <- combined_data %>% 
+
+ggplot(cdf01, aes(Sepal.Width)) + 
+  stat_ecdf(geom = "step", color="purple")
+
+
+
+t <- combined_data %>% 
+  filter(segment == 'ha') %>%
   select(cluster_profile, collection_regionname, TMRCA) %>%
   mutate(tmrca_date = date_decimal(TMRCA) %>% as_date()) %>%
   filter(tmrca_date >= as_date('2019-01-01')) %>%
   arrange(tmrca_date) %>%
-  mutate(n = 1) %>%
   mutate(collection_regionname = case_when(grepl('europe', collection_regionname) ~ 'europe',
                                            grepl('africa', collection_regionname) ~ 'africa',
                                            grepl('asia', collection_regionname) ~ 'asia',
@@ -49,15 +54,40 @@ plt_2a <- combined_data %>%
                                            grepl('south america|southern ocean', collection_regionname) ~ 'south america',
                                            grepl('australia|melanesia', collection_regionname) ~ 'australasia',
                                            .default = collection_regionname)) %>%
+  #dplyr::select(-cluster_profile) %>%
+  summarise(n = n(), .by = c(collection_regionname, tmrca_date)) %>%
   group_by(collection_regionname) %>%
-  mutate(cum_sum = cumsum(n))  %>%
+  mutate(cs = cumsum(n)) %>%
+  ungroup()  %>%
+  select(-n)
+
+
+plt_2a <- expand_grid(tmrca_date = seq(ymd('2019-01-01'),ymd('2024-05-01'),by='day'),
+                      collection_regionname = c('europe', 'africa', 'asia', 'central & northern america'),
+                      cs = NA_real_) %>%
+  rows_patch(t, by = c('collection_regionname', 'tmrca_date')) %>%
+  group_by(collection_regionname) %>%
+  fill(cs) %>%
+  ungroup() %>%
+  mutate(cs = replace_na(cs, 0)) %>%
+  
   ggplot() + 
-  geom_line(aes(x = tmrca_date, y = cum_sum, colour = collection_regionname), size = 1) + 
+  geom_step(aes(x=tmrca_date, y=cs, color=collection_regionname), lwd = 0.8) +
   scale_colour_manual('Continent', values = region_colours, labels = str_to_title) +
   scale_y_continuous('Reassortans (Cumulative)', expand = c(0.01, 0)) + 
-  scale_x_date('Date', expand = c(0.01, 0)) +
+  scale_x_date(limits = as_date(c('2019-01-01', '2024-02-01')),
+                   breaks = '1 year', 
+                   date_labels = "%Y", 'Date (Years)',
+                   expand = c(0,0)) + 
   theme_classic() + 
-  theme(legend.position = 'none')
+  theme(legend.position = 'inside',
+        legend.position.inside = c(0,1),
+        legend.justification.inside = c(0,1),
+        legend.background = element_blank(),
+        legend.text = element_text(size = 8),
+        legend.title = element_text(size = 10),
+        axis.text = element_text(size = 8),
+        axis.title = element_text(size = 10))
 
 
 # 'Diversity' -  the exponent of Shannon entropy 
@@ -68,15 +98,28 @@ plt_2b <- h5_diversity_sliding_window %>%
                      .default = .)) %>%
   mutate(midpoint = (start_point + end_point)/2) %>%
   mutate(midpoint_date = date_decimal(midpoint)) %>%
-  ggplot(aes(y = diversity, x = midpoint_date, colour = continent, fill = continent)) +
+  ggplot(aes(y = diversity, x = midpoint_date, colour = continent, fill = continent,label = str_to_title(continent))) +
   #geom_point() + 
   geom_smooth(method = 'gam' , formula = y ~ s(x, bs = 'cs'), se = TRUE, alpha = 0.1)  +
+  #geom_labelsmooth(text_smoothing = 30, 
+                  # fill = "white",
+                   #formula = y ~ s(x, bs = 'cs'), 
+                   #method = "gam",
+                   #size = 4, 
+                   #linewidth = 0.1,
+                   #boxlinewidth = 0.3) +
   scale_fill_manual('Continent', values = region_colours, labels = str_to_title) + 
   scale_colour_manual('Continent', values = region_colours, labels = str_to_title) +
   scale_y_continuous('Numbers-Equivalent Shanon Entropy', expand = c(0.01, 0)) + 
-  scale_x_datetime('Date', expand = c(0.01, 0)) +
-  theme_classic() + 
-  theme(legend.position = 'none')
+  scale_x_datetime(limits = as_datetime(c('2020-06-01', '2024-02-01')),
+               breaks = '1 year', 
+               date_labels = "%Y", 'Date (Years)',
+               expand = c(0,0)) + 
+  
+  theme_classic()+ 
+  theme(legend.position = 'none',
+        axis.text = element_text(size = 8),
+        axis.title = element_text(size = 10))
 
 
 # Genetic distance between reassortants
@@ -86,6 +129,7 @@ plt_2b <- h5_diversity_sliding_window %>%
 plt_2 <- cowplot::plot_grid(plt_2a, plt_2b, nrow = 1, labels = 'AUTO', align = 'v', axis = 'tb')
 plt_2
 ############################################## WRITE ###############################################
+ggsave('~/Downloads/flu_plots/figure2.jpeg', height = 12.5, width = 25, units = 'cm', dpi = 360)
 
 
 
