043678f31e2346b2fc770fa04774964c0d809b45
James Baxter <39841697+J-Baxter@users.noreply.github.com>
2025-03-24 09:10:21 +0000

    additional formulae for comparison models; extracting transition probabilities

--- scripts/class_model.R
+++ scripts/class_model.R
@@ -61,29 +61,109 @@ class_data <- combined_data %>%
          class, 
          previous_class,
          tmrca_decimal_adjusted,
-         time_since_previous)
+         time_since_previous) %>%
+  
+  # exclude the first reassortant from each continent
+  drop_na(time_since_previous) %>%
+  drop_na(class)
 
 
   
 ggplot(class_data) + geom_bar(aes(x = class, fill = previous_class), position = 'stack') + facet_wrap( ~ collection_regionname)
-ggplot(class_data) + geom_point(aes(x = class, y = time_since_previous), position = 'jitter') + facet_wrap( ~ collection_regionname)
+ggplot(class_data, aes(x = time_since_previous)) + geom_histogram() + 
+  #geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap( class ~ collection_regionname)
+    
 
 
-class_formula <-bf(class ~  collection_regionname + previous_class + time_since_previous)
+class ~ 0 + collection_regionname + time_since_previous 
+
+time_since_previous + previous_class + (1 | collection_regionname:previous_class)
+
 
 class_formula_priors <- get_prior(class_formula,
                                   data = class_data,
                                   family = categorical(link ='logit')) 
 
 
+class_formula_priors$prior <- "normal(0,5)"
 
-diffusionmodel1_priors$prior[1:8] <- "normal(0,5)"
+class_formula_priors$prior[c(1:8, 10:17)] <- "normal(0,5)"
 
 basic_model <- brm(
-  class_formula
+  class_formula,
+  prior = class_formula_priors,
   data = class_data,
   family = categorical(link ='logit'),
   chains = 2, iter = 10000, cores = 2
 )
 
-  
+pp_check(basic_model, ndraws = 100) 
+
+
+new_data = class_data %>% 
+  select(collection_regionname) %>% 
+  distinct() %>% 
+  mutate(time_since_previous = median(class_data$time_since_previous))
+
+# expectation of the posterior
+basic_model %>%
+  epred_draws(newdata = new_data) %>%
+  #filter(.epred >0.01) %>%
+  ggplot(aes(x  = .epred, y = collection_regionname,
+             slab_colour = collection_regionname,
+             slab_fill = collection_regionname)) +
+  stat_halfeye(p_limits = c(0.001, 0.999),
+               point_interval = "median_hdi",
+              # expand = T,
+               #density = 'histogram',
+               .width =  0.95,
+               normalize = "xy") +
+  facet_grid(cols = vars(.category)) 
+
+basic_model %>%
+  avg_slopes(newdata = 'balanced')
+
+basic_model %>%
+  predicted_draws(newdata = new_data %>% filter(previous_class == 'minor')) %>%
+  count(predicted_class = .prediction) %>%
+  mutate(prop = n / sum(n)) %>%
+  ggplot(aes(x = factor(predicted_class), y = n / sum(n))) +
+  geom_col() + 
+  facet_grid(cols = vars(collection_regionname))
+
+
+
+
+# transition probabilities
+test <- basic_model %>%
+  epred_draws(newdata = new_data)
+
+tidy_dag <- test %>%
+  filter(previous_reassortant_class != 'none') %>%
+  filter(.category != 'none') %>%
+  median_hdci() %>%
+  rename(name = previous_reassortant_class, 
+         to = .category) %>% 
+  as_tidy_dagitty() 
+
+tidy_dag %>%
+  mutate(name = case_when(name == 'major'~ 'Mj',
+                          name == 'moderate' ~ "Md",
+                          name == 'minor' ~ 'Mn')) %>%
+  ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_dag_point(aes(alpha = name, colour = collection_regionname)) +
+  geom_dag_text(size = 3) +
+  geom_dag_edges_fan(aes(edge_width = .epred*2,
+                         label = paste0(round(.epred, digits = 3), 
+                                        ' (', round(.lower, digits = 3),
+                                        '-', round(.upper, digits = 3), ')')), 
+                     spread = 5,
+                     label_size = 3,
+                     label_dodge	= 0.5) + 
+  #geom_dag_edges_arc(aes(edge_width = .epred))+ 
+  facet_grid(cols = vars(collection_regionname),
+             labeller = labeller(collection_regionname= str_to_title)) + 
+  scale_alpha_manual(values = c('D' = 1, 'Mj' = 0.7, 'Mn' = 0.5)) +
+  scale_colour_manual(values = region_colours) +
+  theme_dag(legend.position = 'none', base_size = 8)
