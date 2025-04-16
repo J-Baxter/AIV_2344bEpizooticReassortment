512b37112910be7c74042e8a385048ae92f4b80a
James Baxter <james.baxter@ed.ac.uk>
2025-04-16 21:08:03 +0100

    add file dimensions and text options; add ppc plot from interpretation script; add qq plot

--- scripts/diffusion_model_evaluation.R
+++ scripts/diffusion_model_evaluation.R
@@ -30,7 +30,7 @@ library(ggmcmc)
 ######################################### DATA & MODEL #############################################
 fitted_model <- diffusionmodel1_fit_gamma_19
 
-diffusion_data <- 
+diffusion_data <- diffusion_data
 ###################################### MCMC Diagnostics #############################################
 
 #MCMC chain convergence (Visual Inspection)
@@ -93,9 +93,19 @@ ggs(diffusionmodel1_fit_gamma_19) %>%
              strip.position = 'left',
              labeller = label_parsed)+
   scale_colour_brewer(palette = 'GnBu', 'Chains') +
-  theme_minimal(base_size = 8) + 
-  theme(legend.position = 'bottom')
+  scale_x_continuous('Iteration') + 
+  scale_y_continuous('Parameter Value') + 
+  theme_minimal() + 
+  theme(legend.position = 'bottom',
+        axis.text = element_text(size = 8),
+        axis.title = element_text(size = 10),
+        legend.text = element_text(size = 8))
 
+ggsave('~/Downloads/flu_plots/diffusion_trace.jpeg',
+       dpi = 360,
+       height = 38,
+       width = 20,
+       units = 'cm')
 
 # Plot ranked traces
 # If chains are exploring the same space efficiently, the traces should be similar to one another 
@@ -287,7 +297,17 @@ diffusionmodel1_fit_gamma_19 %>%
   geom_path() + 
   facet_wrap(~Parameter, labeller = label_parsed) + 
   theme_minimal()  + 
-  scale_colour_brewer()
+  scale_colour_brewer('Chain') +
+  theme(legend.position = 'bottom',
+        axis.text = element_text(size = 8),
+        axis.title = element_text(size = 10),
+        legend.text = element_text(size = 8))
+
+ggsave('~/Downloads/flu_plots/diffusion_autocorrelation.jpeg',
+       dpi = 360,
+       height = 30,
+       width = 30,
+       units = 'cm')
 
 
 # Check Ratio of Effective Population Size to Total Sample Size 
@@ -321,12 +341,60 @@ neff_ratio(diffusionmodel1_fit_gamma_19) %>%
 ###################################### Posterior Checks ############################################
 
 #Posterior predictive check
+pp_check(diffusionmodel1_fit_gamma_19, 
+         ndraws = 100, 
+         type = 'dens_overlay_grouped', 
+         group = 'collection_regionname' ) %>%
+  .$data %>%
+  mutate(value = log1p(value)) %>%
+  #filter(!is_y) %>%
+  ggplot() + 
+  geom_density(aes(x = value, 
+                   group= rep_id, 
+                   alpha = is_y,  
+                   linewidth = is_y, 
+                   colour = is_y), 
+               key_glyph = draw_key_path) + 
+  scale_alpha_manual(values = c('FALSE' = 0.00001, 
+                                'TRUE' = 1)) + 
+  scale_linewidth_manual('Data', values = c('FALSE' = 0.1, 
+                                            'TRUE' = 1),
+                         labels  = c(expression(italic('y')['rep']),
+                                     expression(italic('y'))))+ 
+  scale_colour_manual('Data',values = c('FALSE' = '#cbc9e2', 
+                                        'TRUE' = '#54278f'),
+                      labels  = c(expression(italic('y')['rep']),
+                                  expression(italic('y')))) + 
+  
+  guides(alpha= 'none', 
+         colour=guide_legend()) + facet_wrap(~group, labeller  = as_labeller(str_to_title)) + 
+  scale_x_continuous(expression(paste('Predicted Weighted Diffusion Coefficient (',Km^{2}, ' ', year^{-1}, ')' )),
+                     breaks = log1p(c(0, 10^(seq(from = 2, to = 8, by = 2)))),
+                     labels = expression(0,  1%*%10^{2},  1%*%10^{4},1%*%10^{6},1%*%10^{8}),
+                     limits = c(-0.01, log1p(10^8.5)),
+                     expand = c(0.02,0.02))+
+  scale_y_continuous('Density',expand = c(0,0)) +
+  theme_classic() + 
+  theme(strip.text = element_text(face = 'bold', size = 10),
+        strip.background = element_blank(),
+        legend.title = element_blank(),
+        legend.position = 'bottom',
+        axis.text = element_text(size = 8),
+        axis.title = element_text(size = 10),
+        legend.text = element_text(size = 8))
 
+
+ggsave('~/Downloads/flu_plots/diffusion_ppc.png',
+       dpi = 360,
+       device = 'png' ,
+       height = 17,
+       width = 20, 
+       units = 'cm')
 
 #Step 3B. Summarize posterior of variables
-t <- get_variables(fitted_model)
+t <- get_variables(diffusionmodel1_fit_gamma_19)
 
-beta_draws <- fitted_model %>%
+beta_draws <- diffusionmodel1_fit_gamma_19 %>%
   gather_draws(., !!!syms(t)) %>%
   mutate(type = 'posterior') %>%
   filter(grepl('^b_', .variable)) %>%
@@ -354,11 +422,31 @@ beta_draws <- fitted_model %>%
                            .variable == 'b_shape_collection_regionnameasia'~ "alpha['asia']",
                            .variable == 'b_shape_collection_regionnameafrica'~ "alpha['africa']",
                            .variable == 'b_shape_collection_regionnameeurope'~ "alpha['europe']",
-                           .variable == 'b_shape_collection_regionnamecentral&northernamerica'~ "alpha['americas']"))
+                           .variable == 'b_shape_collection_regionnamecentral&northernamerica'~ "alpha['americas']",
+                           
+                           
+                           .variable == 'sd_segment__Intercept'~ "sigma[gamma]",
+                           .variable == 'sd_collection_regionname__shape_Intercept'~ "sigma[zeta]",
+                           
+                           .variable == 'r_segment[ha,Intercept] '~ "gamma['ha']",
+                           .variable == "r_segment[mp,Intercept]"~ "gamma['mp']",
+                           .variable == "r_segment[np,Intercept]"~ "gamma['np']",
+                           .variable == "r_segment[ns,Intercept]"~ "gamma['ns']",
+                           .variable == "r_segment[nx,Intercept]"~ "gamma['nx']",
+                           .variable == "r_segment[pa,Intercept]"~ "gamma['pa']",
+                           .variable == "r_segment[pb1,Intercept]"~ "gamma['pb1']",
+                           .variable == "r_segment[pb2,Intercept]"~ "gamma['pb2']",
+                           .variable == 'r_collection_regionname__shape[asia,Intercept]'~ "zeta['asia']",
+                           .variable == 'r_collection_regionname__shape[africa,Intercept]'~ "zeta['africa']",
+                           .variable == 'r_collection_regionname__shape[central.&.northern.america,Intercept]'~ "zeta['europe']",
+                           .variable == 'collection_regionname__shape_central.&.northern.america'~ "zeta['central&northernamerica']",
+                           
+                           .variable == 'lprior' ~ 'prior',
+                           .variable == 'lp__' ~ 'log~probability'))
 
 
 
-plt_params <- ggplot() + 
+ ggplot() + 
   geom_histogram(data = beta_draws, 
                  aes(x = .value,
                      y = after_stat(density)),
@@ -506,7 +594,13 @@ plt_params <- ggplot() +
                 alpha = 0.5) +
   xlim(c(-15,15)) + 
   facet_wrap(~label, scales = 'free_y',  ncol = 3, labeller = label_parsed) +
-  theme_minimal()
+  theme_minimal() +
+   theme(axis.text = element_text(size = 8),
+         axis.title = element_text(size = 10),
+         legend.text = element_text(size = 8))
+ 
+
+ 
 
 
 ###################################### Residuals Checks ############################################
@@ -530,10 +624,20 @@ qq_data <- data.frame(
 
 ggplot(qq_data, aes(sample = sample)) +
   stat_qq(distribution = stats::qunif) +
-  stat_qq_line(distribution = stats::qunif) 
+  stat_qq_line(distribution = stats::qunif) +
+  scale_y_continuous('Observed')+
+  scale_x_continuous('Expected')+
+  theme_minimal() +
+  theme(axis.text = element_text(size = 8),
+        axis.title = element_text(size = 10),
+        legend.text = element_text(size = 8))
 
+ggsave('~/Downloads/flu_plots/diffusion_qq.jpeg',
+       dpi = 360,
+       height = 15,
+       width = 15,
+       units = 'cm')
 
-# Residuals vs Predicted
 
 
 ############################################## WRITE ###############################################
