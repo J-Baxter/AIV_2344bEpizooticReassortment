5771512a3fa7c52c6418a5130f77e2340d3eae4c
James Baxter <james.baxter@ed.ac.uk>
2025-05-14 11:19:27 +0100

    plots d, e, f and g

--- scripts/ordinal_model_interpretation.R
+++ scripts/ordinal_model_interpretation.R
@@ -72,15 +72,137 @@ avg_comparisons(ordinal_model, variables = list("cluster_region" = 'pairwise'), 
 
 ############################################## MAIN ################################################
 
-avg_predictions(ordinal_model, by = 'cluster_region') %>%
-get_draws() %>%
+plt_d <- avg_predictions(ordinal_model, by = 'cluster_region') %>%
+  get_draws(shape = "rvar") |>
+  ggplot(aes(y = cluster_region, xdist = rvar, slab_colour = group,
+             slab_fill = group)) + 
+  stat_slabinterval(position = 'dodgejust',
+                    point_interval = "median_hdci",
+                    slab_alpha = 0.7 ,
+                    p_limits = c(0.01, 0.99),
+                    normalize = 'xy',
+                    .width = 0.95) + 
+  scale_fill_brewer(palette = 'Set1', aesthetics = 'slab_fill') + 
+  scale_colour_brewer(palette = 'Set1', aesthetics = 'slab_colour') + 
+  global_theme
+
+
+
+new_data = class_data %>% 
+  dplyr::select(collection_regionname, previous_class) %>% 
+  filter(previous_class != 'none') %>%
+  distinct() %>% 
+  mutate(time_since_previous = mean(class_data$time_since_previous))
+
+epreds <- ordinal_model %>%
+  epred_draws(newdata = new_data) %>%
+  median_hdci() %>%
+  mutate(across(c(.epred, .lower, .upper), ~ ifelse(.epred < 0.005, NA_real_, .x))) %>%
+  mutate(label =  case_when(!is.na(.epred) ~ paste0(round(.epred, digits = 3), 
+                                                    ' (', round(.lower, digits = 3),
+                                                    '-', round(.upper, digits = 3), ')'))) %>%
+  mutate(across(c(.category, previous_class), .fns = ~ str_to_title(.x)))
+
+graph <- avg_predictions(ordinal_model, by = 'parent_class') %>%
   as_tibble() %>%
-  ggplot(aes(x  = group,
-             y = estimate,
-             slab_colour = cluster_region,
-             slab_fill = cluster_region)) + 
-  geom_interval()
+  mutate(across(c(estimate, conf.low, conf.high), ~ ifelse(estimate < 0.005, NA_real_, .x))) %>%
+  mutate(label =  case_when(!is.na(estimate) ~ paste0(round(estimate, digits = 3), 
+                                                      ' (', round(conf.low, digits = 3),
+                                                      '-', round(conf.high, digits = 3), ')'))) %>%
+  mutate(across(c(group, parent_class), .fns = ~ str_to_title(.x))) %>%
   
+  rename(from = parent_class, 
+         to = group) %>% 
+  drop_na(estimate) %>%
+  relocate(from, to) %>%
+ # mutate(across(c(from, to), .fns = ~paste0(.x, "_", collection_regionname))) %>%  # Create unique node IDs for each region
+  as_tbl_graph(directed = TRUE) %>% 
+  activate(nodes) %>%
+  
+  mutate(name = gsub('_.*', '', name))   %>%
+  
+  mutate(lab = case_when(name == 'Major'~ 'Mj',
+                         name == 'Moderate' ~ "Md",
+                         name == 'Minor' ~ 'Mn'))
+
+
+layout <- create_layout(graph, layout = "kk") %>%
+  mutate(x = case_when(name == 'Minor' ~ 1,
+                       name == 'Moderate' ~ 2,
+                       name == 'Major'  ~ 3),
+         y = case_when(name == 'Minor' ~ 1.5,
+                       name == 'Moderate' ~ 2, 
+                       name == 'Major'  ~ 1))
+
+nodes <- layout %>% 
+  dplyr::select(x,y,name) %>%
+  pivot_longer(cols = c(x,y), values_to = 'coord', names_to = 'axis') %>% 
+  pull(name)
+
+self_moderate_span <- ifelse(nodes == "Minor", 90, -90)
+self_moderate_direction <- ifelse(nodes == "Moderate", 45, 270)
+
+
+plt_e <- layout %>%
+  # activate(nodes) %>%
+  # left_join(layout, by = "name") %>%
+  ggraph() + 
+  geom_edge_bend(arrow = arrow(length = unit(4, 'mm')), 
+                 strength = 0.3, 
+                 start_cap = circle(12, 'mm'),
+                 end_cap = circle(12, 'mm'),
+                 angle_calc = 'along',
+                 label_dodge	= unit(3, 'mm'),
+                 aes(label = label,
+                     edge_width = estimate ),
+                 label_size = 3) +
+  geom_edge_loop(arrow = arrow(length = unit(4, 'mm')), 
+                 end_cap = circle(12, 'mm'),
+                 start_cap =  circle(12, 'mm'),
+                 vjust	= 2,
+                 aes(#span = self_moderate_span,
+                     #direction = self_moderate_direction,
+                     strength = 1,
+                     label = label,
+                     edge_width = estimate),
+                 label_size = 3) +
+  geom_node_circle(aes(x0 = x, y0 = y, r = 0.16, fill = name, colour = name)) +
+  
+  #geom_node_point(size = 15, aes(colour = collection_regionname, alpha = name)) +
+  geom_node_text(aes(label = lab),
+                 colour = 'white',
+                 size = 6,
+                 fontface = 'bold') +
+  scale_fill_brewer(palette = 'Set1') + 
+  scale_edge_width_continuous(range = c(0.1, 1.5)) + 
+  theme_void() + 
+  theme(legend.position = 'none', 
+        strip.background = element_blank(),
+        strip.text = element_text(size = 10, face = 'bold')) +
+  scale_x_continuous(expand = c(0,0), limits = c(0,3.5)) + 
+  scale_y_continuous(expand = c(0,0), limits = c(0.5,2.5))
+
+
+
+
+
+plt_f <- avg_predictions(ordinal_model, by = 'segments_changed') %>%
+  as_tibble() %>%
+  ggplot(aes(ymin = conf.low, ymax = conf.high, y = estimate, x = segments_changed, fill = group,
+             colour = group)) + 
+
+  geom_ribbon(aes(colour = NULL), alpha = 0.2) + 
+  geom_line() + 
+  
+  scale_fill_brewer(palette = 'Set1') + 
+  scale_colour_brewer(palette = 'Set1') + 
+  global_theme
+
+
+plt_g <- conditional_effects(ordinal_model, "time_since_last_major", categorical = TRUE) %>%plot()
+
+
+cowplot::plot_grid(plt_d, plt_e, plt_f,plt_g, labels = 'AUTO', ncol = 2, align = 'hv')
 ############################################## WRITE ###############################################
 
 
