b7245cf87cf3859364c69641709b8a650ca3cf00
James <james.baxter@ed.ac.uk>
2025-08-01 15:04:34 +0800

    refactor brms models with new header; remove deprecated code

--- scripts/statistical_models/diffusion_model.R
+++ scripts/statistical_models/diffusion_model.R
@@ -1,24 +1,23 @@
-####################################################################################################
-####################################################################################################
-## Script name: Diffusion Coefficient Model
-##
-## Purpose of script: A Bayesian mixed model to determine associations between variables obtained 
-## from our phylogenetic analysis and difusion coefficient for each reassortant. Variables of interest
-## include: 1. evolutionary rates, 2. persistence time, 3. region of origin, 4. persistence time in 
-## wild birds, and 6. total number of species jumps
-##
-## This script implements a workflow for a zero-inflated lognormal model. 
-##
-## Date created: 2024-xx-xx
-##
-##
-########################################## SYSTEM OPTIONS ##########################################
-#options(scipen = 6, digits = 7) 
-memory.limit(30000000) 
+################################################################################
+## Script Name:        Diffusion Coefficient Model
+## Purpose:           A Bayesian mixed model to determine associations between 
+##                    the diffusion coefficient obtained from our phylogenetic 
+##                    analysis and for each reassortant and 1. region of origin, 
+##                    2. proportion of evolutionary time in wild birds, 
+##                    3. persistence time and 4. total number of  species jumps
+## Author:             James Baxter
+## Date Created:       2024-XX-XX
+################################################################################
 
+############################### SYSTEM OPTIONS #################################
+options(
+  scipen = 6,     # Avoid scientific notation
+  digits = 7      # Set precision for numerical display
+)
+memory.limit(30000000)
 
-########################################## DEPENDENCIES ############################################
-# Packages
+############################### DEPENDENCIES ###################################
+# Load required libraries
 library(tidyverse)
 library(magrittr)
 library(brms)
@@ -31,20 +30,16 @@ library(emmeans)
 library(marginaleffects)
 library(ggmcmc)
 library(performance)
-library(DHARMa)
-# User functions
-#source('./scripts/figure_scripts/plot_settings.R')
 
-############################################## DATA ################################################
+
+################################### DATA #######################################
+# Read and inspect data
 combined_data <- read_csv('./2024Aug18/treedata_extractions/2024-09-20_combined_data.csv')
-summary_data <- read_csv('./2024Aug18/treedata_extractions/summary_reassortant_metadata_20240904.csv') %>%
-  select(-c(cluster_label,
-            clade)) 
 reassortant_ancestral_changes <- read_csv('./reassortant_ancestral_changes.csv')
 reassortant_stratifiedpersistence <- read_csv('./2025Jun10/reassortant_stratifiedpersistence.csv')
 
-############################################## MAIN ################################################
-
+################################### MAIN #######################################
+# Main analysis or transformation steps
 # Data preprocessing
 diffusion_data <- combined_data %>%
   
@@ -79,7 +74,7 @@ diffusion_data <- combined_data %>%
   
   # join host richness
   left_join(summary_data %>% dplyr::select(c(cluster_profile, 
-                                      host_richness)),
+                                             host_richness)),
             by = join_by(cluster_profile)) %>%
   
   # join persistence proportions
@@ -91,15 +86,15 @@ diffusion_data <- combined_data %>%
   mutate(collection_month = date_decimal(TMRCA) %>% format(., "%m") %>% as.integer(),
          collection_year = date_decimal(TMRCA) %>% format(., "%Y") %>% as.integer() %>% factor(levels = c(2017,2018,2019,2020,2021,2022,2023)),
          collection_season = case_when(collection_month %in% c(12,1,2) ~ 'overwintering', 
-                            collection_month %in% c(3,4,5)  ~ 'migrating_spring', # Rename to spring migration
-                            collection_month %in% c(6,7,8)  ~ 'breeding', 
-                            collection_month %in% c(9,10,11)  ~ 'migrating_autumn' # Rename to autumn migration
+                                       collection_month %in% c(3,4,5)  ~ 'migrating_spring', # Rename to spring migration
+                                       collection_month %in% c(6,7,8)  ~ 'breeding', 
+                                       collection_month %in% c(9,10,11)  ~ 'migrating_autumn' # Rename to autumn migration
          )) %>%
   
   # persistence time proportions
   #mutate(across(c('median_anseriformes_wild', 
-                 # 'median_charadriiformes_wild',
-                 # 'median_galliformes_domestic'), .fns= ~ ifelse(.x/persist.time > 1, 1, .x/persist.time ), .names = '{.col}_prop')) %>%
+  # 'median_charadriiformes_wild',
+  # 'median_galliformes_domestic'), .fns= ~ ifelse(.x/persist.time > 1, 1, .x/persist.time ), .names = '{.col}_prop')) %>%
   
   
   # binary
@@ -123,33 +118,16 @@ diffusion_data <- combined_data %>%
                 segment,
                 cluster_class,
                 collection_regionname) %>%
-
+  
   filter(weighted_diff_coeff > 0) %>%
   filter(segment == 'ha') %>%
   drop_na()
-  
+
 diffusion_data %<>%
   mutate(collection_regionname = case_when(cluster_profile == '2_1_1_1_1_1_1_1' ~ 'europe', .default = collection_regionname))
 
 
 # Model Formula
-# We assume a hurdle lognormal model, in which the hurdle process is determined the season in which 
-# the TMRCA is estimated to be, and the lognormal component is determined by persistence
-# in anseriformes and charadriiformes. Both model components are conditional on segment from which 
-# the measurement is taken and region of origin
-
-#diffusion_formula <- bf(weighted_diff_coeff ~ 1 + median_anseriformes_wild +  median_charadriiformes_wild + collection_regionname + 
-                       #   (1|segment),
-                       # hu ~ 1 + collection_regionname +  season +(1|segment ))
-
-#diffusion_formula <- bf(weighted_diff_coeff|trunc(lb=0) ~ 1 + median_anseriformes_wild +  median_charadriiformes_wild + collection_regionname + (1|segment) + (collection_regionname|collection_year/collection_season))
-#diffusion_formula <- bf(weighted_diff_coeff ~ 0 +collection_regionname + median_anseriformes_wild + median_charadriiformes_wild +  collection_season + (1|segment),
-                     #   shape ~ 0 +  collection_regionname + (1 | segment))
-#diffusion_formula <- bf(weighted_diff_coeff ~ 0 + median_anseriformes_wild +  median_charadriiformes_wild + collection_regionname + collection_season +  (1|segment))
-
-#
-#shape ~ 0 + collection_regionname
-
 diffusion_formula <- bf(weighted_diff_coeff ~ 0 + collection_regionname +
                           count_cross_species_log1p + 
                           persistence + 
@@ -157,19 +135,13 @@ diffusion_formula <- bf(weighted_diff_coeff ~ 0 + collection_regionname +
                           path_prop_anseriformes_wild + 
                           path_prop_charadriiformes_wild + 
                           path_prop_galliformes_domestic + 
-
+                          
                           ( path_prop_anseriformes_wild + persistence| collection_regionname) + 
                           (1 | collection_year) ,
                         shape ~  0 + collection_regionname )
 
 
-
-# Define Priors
- get_prior(diffusion_formula,
-           data = diffusion_data,
-           family = Gamma(link = "log")) 
-
-
+# Set Priors
 diffusionmodel1_priors <- c(set_prior("normal(0, 2)", class = 'b'),
                             set_prior('normal(13,2)', class = 'b', coef = 'collection_regionnameafrica'),
                             set_prior('normal(13,1.5)', class = 'b', coef = 'collection_regionnameasia'),
@@ -190,29 +162,8 @@ BURNIN <- ITER/10 # Discard 10% burn in from each chain
 SEED <- 4472
 
 
-
-# Prior Predictive Checks 
-# Note that due to weakly informative priors, the prior predictive checks reveal little about the 
-# ability of our priors to describe the data. 
-
-diffusionmodel1_prior <- brm(
-  diffusion_formula,
-  data = diffusion_data,
-  family = Gamma(link = "log"),
-  prior = diffusionmodel1_priors,
-  sample_prior = "only",
-  chains = 2,
-  threads = 2, 
-  cores = 2, 
-  iter = ITER,
-  warmup = BURNIN,
-  seed = SEED,
-  control = list(adapt_delta = 0.95)
-)
-
-
 # Fit model to data
-diffusionmodel1_fit_gamma_19<- brm(
+diffusion_model<- brm(
   diffusion_formula,
   data = diffusion_data,
   prior = diffusionmodel1_priors,
@@ -228,11 +179,11 @@ diffusionmodel1_fit_gamma_19<- brm(
 
 
 # Post-fitting checks (including inspection of ESS, Rhat and posterior predictive)
-performance(diffusionmodel1_fit_gamma_19)
+performance(diffusion_model)
 
 
-saveRDS(diffusionmodel1_fit, './saved_models/diffusion_model_2.rds')
-write_csv(diffusion_data, './saved_models/diffusion_model.csv')
-############################################## END #################################################
-####################################################################################################
-####################################################################################################
+################################### OUTPUT #####################################
+# Save output files, plots, or results
+
+#################################### END #######################################
+################################################################################
--- scripts/statistical_models/ordinal_model.R
+++ scripts/statistical_models/ordinal_model.R
@@ -1,20 +1,20 @@
-####################################################################################################
-####################################################################################################
-## Script name: Class Model (using cumulative distribution)
-##
-## Purpose of script:to model the probability that any given reassortant belongs to class X,
-## stratified by continent
-##
-## Date created: 2025-05-13
-##
-##
-########################################## SYSTEM OPTIONS ##########################################
-options(scipen = 6, digits = 7) 
-memory.limit(30000000) 
+################################################################################
+## Script Name:         Class Model (using cumulative distribution)
+## Purpose:            to model the probability that any given reassortant belongs 
+##                     to class X, stratified by continent.
+## Author:             James Baxter
+## Date Created:       2025-05-13
+################################################################################
 
-  
-########################################## DEPENDENCIES ############################################
-# Packages
+############################### SYSTEM OPTIONS #################################
+options(
+  scipen = 6,     # Avoid scientific notation
+  digits = 7      # Set precision for numerical display
+)
+memory.limit(30000000)
+
+############################### DEPENDENCIES ###################################
+# Load required libraries
 library(tidyverse)
 library(magrittr)
 library(broom)
@@ -23,11 +23,8 @@ library(brms)
 library(cmdstanr)
 library(igraph)
 
-# User functions
-
-
-############################################## DATA ################################################
-
+################################### DATA #######################################
+# Read and inspect data
 reassortant_ancestral_changes <- read_csv('./reassortant_ancestral_changes.csv')%>%
   mutate(across(ends_with('class'), .fns = ~ case_when(.x == 1 ~ 'moderate',
                                                        .x == 2 ~ 'minor',
@@ -36,10 +33,12 @@ reassortant_ancestral_changes <- read_csv('./reassortant_ancestral_changes.csv')
 combined_data <- read_csv('./2024Aug18/treedata_extractions/2024-09-20_combined_data.csv')
 summary_data <- read_csv('./2024Aug18/treedata_extractions/summary_reassortant_metadata_20240904.csv') %>%
   dplyr::select(-c(cluster_label,
-            clade)) 
+                   clade)) 
 meta <- read_csv('./2024-09-09_meta.csv')
 
-# Obtain 'nearest major reassortant'
+
+################################### MAIN #######################################
+# Main analysis or transformation steps
 
 my_edgelist<- reassortant_ancestral_changes %>% 
   dplyr::select(ends_with('label'), cluster_class) %>%
@@ -50,7 +49,10 @@ plot(my_edgelist, vertex.size = 7)
 
 
 most_recent_major <- distances(my_edgelist,
-                               weights = reassortant_ancestral_changes %>%  dplyr::select(ends_with('label'), segments_changed) %>% drop_na() %>% pull(segments_changed),
+                               weights = reassortant_ancestral_changes %>%  
+                                 dplyr::select(ends_with('label'), segments_changed) %>% 
+                                 drop_na() %>% 
+                                 pull(segments_changed),
                                mode = 'in',
                                to =  c( "H5N8/2019/R7_Africa",
                                         "H5N1/2020/R1_Europe",
@@ -72,15 +74,12 @@ most_recent_major <- distances(my_edgelist,
   left_join(meta %>% dplyr::select(last_major_profile = cluster_profile, last_major_label = cluster_label) %>% distinct())
 
 
-
-####################################################################################################
-# Update reassortant 'ancestry'
 updated <- reassortant_ancestral_changes %>%
   
   # Add  origin continent of current reassortant
   left_join(combined_data %>% filter(segment == 'ha') %>% 
               dplyr::select(cluster_profile,
-                     cluster_region = collection_regionname) %>% 
+                            cluster_region = collection_regionname) %>% 
               group_by(cluster_profile) %>% 
               #slice_min(cluster_tmrca, n = 1) %>% 
               distinct()) %>%
@@ -89,7 +88,7 @@ updated <- reassortant_ancestral_changes %>%
   left_join(combined_data %>% 
               filter(segment == 'ha') %>% 
               dplyr::select(cluster_profile, 
-                     parent_region = collection_regionname) %>% 
+                            parent_region = collection_regionname) %>% 
               group_by(cluster_profile) %>% 
               distinct(),
             by = join_by(parent_profile == cluster_profile), 
@@ -101,8 +100,8 @@ updated <- reassortant_ancestral_changes %>%
   # Add 'last' (with respect to HA) major reassortant
   left_join(most_recent_major) %>%
   left_join(., dplyr::select(., last_major_profile = cluster_profile, 
-                      last_major_region = cluster_region, 
-                      last_major_tmrca = cluster_tmrca)) %>%
+                             last_major_region = cluster_region, 
+                             last_major_tmrca = cluster_tmrca)) %>%
   
   mutate(across(ends_with('tmrca'), .fns = ~subtract(2024.21, .x))) %>%
   
@@ -138,68 +137,57 @@ updated %<>%
 
 class_data <- updated %>%
   dplyr::select(cluster_class,
-         parent_class,
-         cluster_region, 
-         segments_changed,
-         region_changed_from_previous,
-         time_since_last_major,
-         time_since_parent,
-         rnp_changed) %>%
+                parent_class,
+                cluster_region, 
+                segments_changed,
+                region_changed_from_previous,
+                time_since_last_major,
+                time_since_parent,
+                rnp_changed) %>%
   
   mutate(cluster_class = ordered(cluster_class, levels = c('minor', 'moderate', 'major')))
 
-############################################## MAIN ################################################
+
 # Formula
 ordinal_formula_priors <- get_prior(cluster_class~1 + parent_class + cluster_region + s(time_since_last_major) + segments_changed,
-                                  data = class_data,
-                                  family = cumulative("probit")) 
+                                    data = class_data,
+                                    family = cumulative("probit")) 
 
 
 
-# Priors
+# Set Priors
 ordinal_formula_priors <- c(set_prior("normal(0, 2)", class = 'b'),
                             set_prior('exponential(0.5)', class = c('sds')))
 
 
+# Set MCMC Options
 CHAINS <- 4
 CORES <- 4
 ITER <- 2000
 BURNIN <- ITER/10 # Discard0% burn in from each chain
 SEED <- 4472
 
+
+# Fit model to data
 ordinal_model <- brm(cluster_class~1 + parent_class + cluster_region + s(time_since_last_major) + segments_changed,
-            data=class_data,
-            prior = ordinal_formula_priors,
-            family =cumulative("probit"),
-            chains = CHAINS,
-            cores = CORES, 
-            threads = 2, 
-            backend = "cmdstanr",
-            iter = ITER,
-            warmup = BURNIN,
-            seed = SEED)
-
-#marginal_effects(temp, "parent_class", categorical = TRUE) #Graph
-#marginal_effects(ordinal_model, "time_since_last_major", categorical = TRUE)
-#marginal_effects(temp, "cluster_region", categorical = TRUE)
-#marginal_effects(temp, "region_changed_from_previous", categorical = TRUE) # no difference
-
-############################################## WRITE ###############################################
+                     data=class_data,
+                     prior = ordinal_formula_priors,
+                     family =cumulative("probit"),
+                     chains = CHAINS,
+                     cores = CORES, 
+                     threads = 2, 
+                     backend = "cmdstanr",
+                     iter = ITER,
+                     warmup = BURNIN,
+                     seed = SEED)
 
 
-simulatedResponse = ordinal_model %>% 
-  predict(type="response") %>% 
-  apply(1, \(x) sample(1:length(x), prob = x, size=1000, replace = TRUE)) %>% 
-  t()
-model.check <-createDHARMa(
-  simulatedResponse = simulatedResponse,
-  observedResponse = as.integer(class_data %>% drop_na() %>% pull(cluster_class)),
-  fittedPredictedResponse = ordinal_model %>% predict(), # the linear predictor
-  integerResponse = !all(as.numeric(simulatedResponse)%%1!=0) # checks if prediction consists of integers
-)
+# Post-fitting checks (including inspection of ESS, Rhat and posterior predictive)
+performance(ordinal_model)
 
-plot(model.check)
 
-############################################## END #################################################
-####################################################################################################
-####################################################################################################
+################################### OUTPUT #####################################
+# Save output files, plots, or results
+
+#################################### END #######################################
+################################################################################
