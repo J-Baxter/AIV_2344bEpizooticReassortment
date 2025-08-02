3222a7103b3f1b1bc5bd9d7caafa043ebb3927ef
James <james.baxter@ed.ac.uk>
2025-08-02 17:12:36 +0800

    Refactored to present header. Additional documentation to explain the origin of objects called later, but are extracted from file-paths in csv_data

--- scripts/tree_data_analysis.R
+++ scripts/tree_data_analysis.R
@@ -1,11 +1,29 @@
-# Dependencies
+################################################################################
+## Script Name:        Tree Data Extraction
+## Purpose:            Update combined data with persistence, tmrca and evo.rate
+##                    estimates from current analysis
+## Author:             James Baxter
+## Date Created:       2025-08-02
+################################################################################
+
+############################### SYSTEM OPTIONS #################################
+options(
+  scipen = 6,     # Avoid scientific notation
+  digits = 7      # Set precision for numerical display
+)
+memory.limit(30000000)
+
+############################### DEPENDENCIES ###################################
+# Load required libraries
 library(tidyverse)
+library(magrittr)
 library(beastio)
 
-# Import Tree Data Extractions
+################################### DATA #######################################
+# Read and inspect data
 csv_paths <- list.files("./2024Aug18/treedata_extractions",
-  pattern = ".csv",
-  full.names = TRUE
+                        pattern = ".csv",
+                        full.names = TRUE
 )
 
 csv_names <- gsub(
@@ -14,17 +32,30 @@ csv_names <- gsub(
   csv_paths
 )
 
+
+# Import Tree Data Extractions
 csv_data <- lapply(csv_paths, read_csv) %>%
   setNames(csv_names)
 
-# Expand to separate objects
+# Expand to separate objects - this includes those called later:
+# >summary_reassortant_metadata_20240904,
+# >reassortant_stratifiedpersistence,
+# >RegionalTre_mrca_stats_disp_jumps_combined
+#>2024-09-20_combined_data
+#>domainantTre_mrca_stats_disp_jumps_combined
 for (i in 1:length(csv_data)) {
   assign(names(csv_data)[i], as_tibble(csv_data[[i]]))
 }
 
+reassortant_log_path <- list.files("./2024Nov18/plain_tree",
+                                   pattern = '.log',
+                                   full.names = T)
 
-################### Combined Tibble ###################
 
+################################### MAIN #######################################
+# Main analysis or transformation steps
+
+## Import Evolutionary Rates from Reassortant log files
 combined_data <- RegionalTre_mrca_stats_disp_jumps_combined %>%
   # remove dominant cluster profiles
   filter(!Rcode %in% c(
@@ -46,16 +77,16 @@ combined_data <- RegionalTre_mrca_stats_disp_jumps_combined %>%
   select(-c(
     cluster_number,
     collection_countryname,
-
+    
     prob_cluster_number,
     prob_collection_countryname,
-
+    
   )) %>%
   # bind results from dominant trees
   bind_rows(domainantTre_mrca_stats_disp_jumps_combined) %>%
   # bind summary table
   left_join(
-    summary_reassortant_metadata_20240904 %>%
+    summary_reassortant_metadata_20240904 %>% ###
       select(c(
         cluster_profile,
         col2,
@@ -68,20 +99,14 @@ combined_data <- RegionalTre_mrca_stats_disp_jumps_combined %>%
   # split labels to extract segments
   separate_wider_delim(data_name, "_", names = c("segment", "deprecated")) %>%
   mutate(cluster_profile = case_when(deprecated == "11111111A" & Rcode == "1_1_1_1_1_1_1_1" ~ "1_1_1_1_1_1_1_1A",
-    .default = Rcode
+                                     .default = Rcode
   )) %>%
   select(-c(deprecated, Rcode)) %>%
   mutate(segment = gsub("n[:0-9:]", "nx", segment)) %>%
   relocate(cluster_profile, segment)
 
 
-################### Import Evolutionary Rates from Reassortant log files ###################
-reassortant_log_path <- list.files("./2024Nov18/plain_tree",
-                                   pattern = '.log',
-                                   full.names = T)
-
-
-
+# Import Evolutionary Rates from Reassortant log files 
 imported_logs <- lapply(reassortant_log_path, readLog) %>%
   lapply(., getHPDMedian) %>%
   lapply(., function(x) as_tibble(x, rownames = 'parameter') %>% filter(parameter %in% c("ucld.mean", "age.root."))) %>%
@@ -98,30 +123,36 @@ imported_logs <- lapply(reassortant_log_path, readLog) %>%
   rename_with(.fn = ~gsub('_med', '', .x) , .cols = contains('med'))
 
 
-################### Import Evolutionary Rates from Reassortant log files ###################
 reassortant_stratifiedpersistence %<>%
   rename_with(.fn = ~gsub('persistence_host_|_simplifiedhost', '', .x)) %>%
   pivot_wider(names_from = host,
               values_from = c(median, max, sum),
               names_sep = '_',
-              values_fn = median) #requires debugging
+              values_fn = median) 
 
-################### Combine all data ###################
+
+################################### OUTPUT #####################################
+# Save output files, plots, or results
+# Combine all data
 combined_data <- combined_data %>%
   unite(key, segment, cluster_profile, remove = F) %>%
   mutate(key = str_remove_all(key, "(?<=_\\d{1,2})_")) %>%
   
   
   rows_update(.,
-    imported_logs %>% dplyr::select(- c(TMRCA_95lower , TMRCA_95upper)),
-    by = "key",
-    unmatched = "ignore"
+              imported_logs %>% dplyr::select(- c(TMRCA_95lower , TMRCA_95upper)),
+              by = "key",
+              unmatched = "ignore"
   ) %>%
   # join reassortant-stratified host persistence time
-   
+  
   left_join(.,
             y = reassortant_stratifiedpersistence) %>%
   select(-contains('+'))
 
 
 write_csv(combined_data, './2024Aug18/treedata_extractions/2024-09-20_combined_data.csv')
+
+
+#################################### END #######################################
+################################################################################
